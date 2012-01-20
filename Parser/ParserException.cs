8ecb69279c0797a914d539c766681082fd3adc31
kosta <kosta@kosta-HP.vsu.edu>
2012-01-20 09:56:51 -0500

    Added srcML binaries and initial set of changes to Parser

---
+++ Parser/ParserException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sando.Parser { 
+
+	class ParserException : System.Exception
+	{
+		public ParserException(string message)
+        : base(message)
+		{
+		}
+	}
+
+}
