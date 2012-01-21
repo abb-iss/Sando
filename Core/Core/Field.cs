e86e3099d7318b827a4af922890ac3dc57b8b2f6
davidshepherd <davidshepherd@gmail.com>
2012-01-21 13:08:10 -0500

    Adding Field class

---
+++ Core/Core/Field.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sando.Core
+{
+	class Field: ProgramElement
+	{
+		public override string ContainerName
+		{
+			get { throw new NotImplementedException(); }
+			set { throw new NotImplementedException(); }
+		}
+	}
+}
