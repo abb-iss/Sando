85ece2eca53a2819e168e89863ee7586b8d9aba9
davidshepherd <davidshepherd@gmail.com>
2012-01-20 20:33:57 -0500

    Adding some tests for the parser

--- Parser/Parser.UnitTests/HelloWorldParserTest.cs
+++ Parser/Parser.UnitTests/HelloWorldParserTest.cs
@@ -8,23 +8,41 @@ namespace Sando.Parser.UnitTests
 	public class HelloWorldParserTest
 	{
 		static String HelloWorldFile;
+		private static string CurrentDirectory;
+		private static SrcMLGenerator Generator;
 
 		[SetUp]
 		public static void Init()
 		{
+			//Note: may not want to create this in mydocuments.... 
 			//create a test file
 			String HelloWorld = "public class Hello1 { public static void Main() { System.Console.WriteLine(\"Hello, World!\"); } }";
 			HelloWorldFile = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\HelloWorld1.cs";
 			System.IO.File.WriteAllText(HelloWorldFile, HelloWorld);
+			//set up generator
+			CurrentDirectory = Environment.CurrentDirectory;
+			Generator = new SrcMLGenerator();
+			Generator.SetSrcMLLocation(CurrentDirectory + "\\..\\..\\..\\..\\LIBS\\srcML-Win");
 		}
 
 		[Test]
-		public void GenerateSrcMLTest()
+		public void GenerateSrcMLBasicFileTest()
 		{
-			var generator = new SrcMLGenerator();
-			var v = Environment.CurrentDirectory;
-			generator.SetSrcMLLocation(v + "\\..\\..\\..\\..\\LIBS\\srcML-Win");
-			String srcML = generator.GenerateSrcML(HelloWorldFile);
+			String srcML = Generator.GenerateSrcML(HelloWorldFile);
+			Assert.IsNotNullOrEmpty(srcML);
+		}
+
+		[Test]
+		public void GenerateSrcMLShortFileTest()
+		{
+			String srcML = Generator.GenerateSrcML("..\\..\\TestFiles\\ShortCSharpFile.txt");
+			Assert.IsNotNullOrEmpty(srcML);
+		}
+
+		[Test]
+		public void GenerateSrcMLShortestFileTest()
+		{			
+			String srcML = Generator.GenerateSrcML("..\\..\\TestFiles\\ShortestCSharpFile.txt");
 			Assert.IsNotNullOrEmpty(srcML);
 		}
 
