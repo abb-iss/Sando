bdf84239a2a48effc525f8b21462de61f9cbb391
davidshepherd <davidshepherd@gmail.com>
2012-01-21 21:59:58 -0500

    Adding comments to help creating splitter + tests

--- Parser/Parser.UnitTests/SplitterTest.cs
+++ Parser/Parser.UnitTests/SplitterTest.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
 
-namespace Parser.UnitTests
+namespace Sando.Parser.UnitTests
 {
+	[TestFixture]
 	class SplitterTest
 	{
+		[Test]
+		public void TestSplitter()
+		{
+			//Splitter split = new Splitter();
+			//String[] parts = split.split("aLongVariableNameInCamelCase");
+			//Assert.IsTrue(parts.Length()==7);
+		}
 	}
 }
