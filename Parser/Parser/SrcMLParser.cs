ea4165b1411707cdbd2b4d617f1465cd1f21636f
kosta <kosta@kosta-HP.vsu.edu>
2012-01-23 15:20:38 -0500

    catching up with what dave recently changed in the parser

--- Parser/Parser/SrcMLParser.cs
+++ Parser/Parser/SrcMLParser.cs
@@ -1,6 +1,8 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Sando.Core;
+using System.Xml.Linq;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Sando.Parser
 {
@@ -26,8 +28,57 @@ namespace Sando.Parser
 			String srcml = Generator.GenerateSrcML(filename);
 
 			//now parse the important parts of the srcml and generate program elements
+			XDocument xDoc = XDocument.Parse(srcml);
 
-			return null;
+			var a = from namespc in xDoc.Descendants("decl")
+					where namespc.Element("type").Value == "namespace"
+					select namespc.Element("block");
+
+			String nmspace = "";
+			ProgramElement[] classes = parseClass(xDoc, filename, nmspace);
+
+			return classes;
+		}
+
+		private ProgramElement[] parseClass(XDocument xDoc, String filename, String nmspace) {
+			
+			List<ClassElement> classElements = 
+				(from cls in xDoc.Descendants("class")
+				 select new ClassElement {
+					Namespace = nmspace,
+					FileName = filename,
+					Name = cls.Element("name").Value,
+					DefinitionLineNumber = Int32.Parse(cls.Attribute("pos::line").Value),
+					AccessLevel = strToAccessLevel(cls.Element("specifier").Value)
+				 }).ToList<ClassElement>();
+
+			var Elements = from cls in xDoc.Descendants()
+						   select cls;
+
+			//foreach(String name in classNames)
+			//{
+			//    ClassElement classElement = new ClassElement();
+			//    classElement.Name = name;
+			//    classElement.FileName = filename;
+
+			//}
+
+			return classElements.ToArray();
+		}
+
+		private AccessLevel strToAccessLevel(String level) 
+		{
+			switch(level.ToLower())
+			{
+				case "private":
+					return AccessLevel.Private;
+				case "public":
+					return AccessLevel.Public;
+				case "protected":
+					return AccessLevel.Protected;
+				default:
+					return AccessLevel.Protected;			
+			}
 		}
 
 	}
