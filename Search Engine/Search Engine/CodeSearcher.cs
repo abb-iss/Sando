afe56ae0cf252375813bcef667a83951359b42c2
manickavasagam <manickavasagam.sun>
2012-01-24 22:35:17 -0800

    Defined a basic searcher. The searcher uses Lucene.net classes which will be obsolete in v3.0

---
+++ Search Engine/Search Engine/CodeSearcher.cs
@@ -0,0 +1,77 @@
+﻿namespace Sando.SearchEngine
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Lucene.Net;
+    using Lucene.Net.Analysis;
+    using Lucene.Net.Documents;
+    using Lucene.Net.Index;
+    using Lucene.Net.QueryParsers;
+    using Lucene.Net.Search;
+    using Lucene.Net.Store;
+    public class CodeSearcher
+    {
+        #region Private Members
+        /// <summary>
+        /// private member for lucene directory
+        /// </summary>
+        private string directory;
+
+        /// <summary>
+        /// private member for analyzer
+        /// </summary>
+        private Analyzer analyzer;        
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CodeSearcher"/> class.
+        /// </summary>
+        /// <param name="directory">Sando code indexer directory.</param>
+        /// <param name="analyzer">Analyzer used during index creation, Same has to be used during search.</param>
+        public CodeSearcher(string directory, SimpleAnalyzer analyzer)
+        {
+            this.directory = directory;
+            this.analyzer = analyzer;            
+        }
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Searches the specified search string.
+        /// </summary>
+        /// <param name="searchString">The search string.</param>
+        public virtual Hits Search(string searchString)
+        {
+            Directory luceneDirectory = null;            
+            IndexSearcher searcher = null;
+            try
+            {
+             luceneDirectory = FSDirectory.Open(new System.IO.DirectoryInfo(this.directory));             
+             searcher = new IndexSearcher(luceneDirectory, true);             
+             Query searchQuery = MultiFieldQueryParser.Parse(Lucene.Net.Util.Version.LUCENE_29, new string[] { searchString }, this.GetSearchableFields(), this.analyzer);
+             return searcher.Search(searchQuery);             
+            }
+            finally
+            {
+                if (luceneDirectory != null)
+                {
+                    luceneDirectory.Close();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the searchable fields.
+        /// </summary>
+        /// <returns>An array of searchable fields</returns>
+        public virtual string[] GetSearchableFields()
+        {
+            //TODO : To determine logic to get searchable fields for different elements
+            return new string[] {"Name"};
+        }
+        #endregion
+    }
+}
---
+++ Search Engine/SearchEngine.UnitTests/CodeSearcherFixture.cs
@@ -0,0 +1,64 @@
+﻿
+namespace Sando.SearchEngine.UnitTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using NUnit.Framework;
+    using Sando.SearchEngine;
+    using Lucene.Net.Analysis;
+    using Sando.Core;
+    using Sando.Indexer;
+    using Sando.Indexer.Documents;
+    using Lucene.Net.Search;
+    [TestFixture]
+    public class CodeSearcherFixture
+    {
+        [Test]
+        public void TestCreateCodeSearcher()
+        {
+            SimpleAnalyzer analyzer = new SimpleAnalyzer();           
+            Assert.DoesNotThrow(() => new CodeSearcher( System.IO.Path.GetTempPath() +"luceneindexer", analyzer));
+        }
+
+        [Test]     
+        public void PerformBasicSearch()
+        {
+            Analyzer analyzer = new SimpleAnalyzer();            
+            string indexerPath = System.IO.Path.GetTempPath() +"luceneindexer";
+            DocumentIndexer target = new DocumentIndexer(indexerPath, analyzer);
+            ClassElement classElement = new ClassElement()
+            {
+                AccessLevel = Core.AccessLevel.Public,
+                DefinitionLineNumber = 11,
+                ExtendedClasses = "SimpleClassBase",
+                FileName = "SimpleClass.cs",
+                FullFilePath = "C:/Projects/SimpleClass.cs",
+                Id = Guid.NewGuid(),
+                ImplementedInterfaces = "IDisposable",
+                Name = "SimpleName",
+                Namespace = "Sanod.Indexer.UnitTests"
+            };
+            SandoDocument sandoDocument = ClassDocument.Create(classElement);
+            target.AddDocument(sandoDocument);
+            MethodElement methodElement = new MethodElement()
+            {
+                AccessLevel = Core.AccessLevel.Protected,
+                Name = "SimpleName",
+                Id = Guid.NewGuid(),
+                ReturnType="Void",
+                ClassId=Guid.NewGuid()
+            };
+            sandoDocument = MethodDocument.Create(methodElement);
+            target.AddDocument(sandoDocument);
+            target.CommitChanges();
+            CodeSearcher cs = new CodeSearcher(indexerPath, new SimpleAnalyzer());            
+            Hits hits = cs.Search("SimpleName");
+            Assert.AreEqual(2, hits.Length());
+            target.Dispose();
+            System.IO.File.Delete(indexerPath);
+            Assert.Pass("Passed.Luncene Index Directory no longer used after Indexer and Searcher disposed");
+        }
+    }
+}
