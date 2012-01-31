ed75fbc0206b5165737f57065d8c04c02bff8173
manickavasagam <manickavasagam.sun>
2012-01-30 16:07:31 -0800

    Removed Lucene.net references from the search engine project fixed some styling issues

--- Search Engine/Search Engine/CodeSearchResult.cs
+++ Search Engine/Search Engine/CodeSearchResult.cs
@@ -1,42 +1,49 @@
-﻿using Sando.Core;
-
-namespace Sando.SearchEngine
+﻿namespace Sando.SearchEngine
 {
+    using Sando.Core;
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
-    using Lucene.Net.Documents;
+    using System.Text;    
 
     /// <summary>
     /// Class defined to create return result from Lucene indexer
     /// </summary>
    public class CodeSearchResult
-    {
-
-	   public CodeSearchResult(ProgramElement programElement, double p)
-	   {
-		   this.Element = programElement;
-		   this.Score = p;
-	   }
-        /// <summary>
-        /// Gets or sets the score.
-        /// </summary>
-        /// <value>
-        /// The search score.
-        /// </value>
+   {
+       #region Public Properties
+       /// <summary>
+       /// Gets or sets the score.
+       /// </summary>
+       /// <value>
+       /// The search score.
+       /// </value>
        public double Score { get; private set; }
 
        /// <summary>
        /// Gets or sets the element.
        /// </summary>
        /// <value>
-       /// The Lucene document.
+       /// Sando Program Element.
        /// </value>
-	   public ProgramElement Element
+       public ProgramElement Element
+       {
+           get;
+           private set;
+       }
+       #endregion
+       #region Constructor
+       /// <summary>
+        /// Initializes a new instance of the <see cref="CodeSearchResult"/> class.
+        /// </summary>
+        /// <param name="programElement">program element.</param>
+        /// <param name="score">search score.</param>
+	   public CodeSearchResult(ProgramElement programElement, double score)
 	   {
-		   get;
-		   private set;
+		   this.Element = programElement;
+		   this.Score = score;
 	   }
+       #endregion
+       
     }
 }
