ed3d3665d159fc71d54ad908e7e6c91679709ed0
kostata <devnull@localhost>
2012-02-13 12:53:31 -0500

    Adding first iteration of word splitter in parser

---
+++ Parser/Parser/WordSplitter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Sando.Parser
+{
+	public static class WordSplitter
+	{
+		public static string[] split(string word) 
+		{
+			List<string> splits = new List<string>();
+
+			word = camelTypeToUnderscore(word);
+
+			if(word.Contains("_"))
+			{
+				splits.AddRange(SplitUnderscores(word).ToList());
+			}
+
+			return splits.ToArray();
+		}
+
+		private static string camelTypeToUnderscore(string word)
+		{
+			return Regex.Replace(word, @"([A-Z][a-z]+)", "_$1");
+		}
+
+		private static string[] SplitUnderscores(string word) 
+		{
+			char[] delimiters = new char[] { '_' };
+			return word.Split(delimiters,StringSplitOptions.RemoveEmptyEntries);
+		}
+
+
+	}
+}
