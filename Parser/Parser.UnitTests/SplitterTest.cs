898f6da571dc0b6d7252bde67cf19e224c94f72d
davidcshepherd <devnull@localhost>
2012-02-14 09:01:41 -0500

    Updated SplitterTests to check for abbreviations.

--- Parser/Parser.UnitTests/SplitterTest.cs
+++ Parser/Parser.UnitTests/SplitterTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
@@ -28,6 +29,49 @@ namespace Sando.Parser.UnitTests
 		{
 			string[] parts = WordSplitter.split("unsplittable");
 			Assert.IsTrue(parts.Length == 1);
+		}
+
+		[Test]
+		public void TestAbbreviations()
+		{
+			string[] parts = WordSplitter.split("whatAboutALM");
+			Assert.IsTrue(parts.Length == 3);
+		}
+
+		[Test]
+		public void TestAllCaps()
+		{
+			string[] parts = WordSplitter.split("WHATIFALLINCAPS");
+			Assert.IsTrue(parts.Length == 1);
+		}
+
+		[Test]
+		public void TestAllCapsUnderscore()
+		{
+			string[] parts = WordSplitter.split("WHAT_IF_ALL_IN_CAPS");
+			Assert.IsTrue(parts.Length == 5);
+		}
+
+		[Test]
+		public void TestBeginUnderscore()
+		{
+			string[] parts = WordSplitter.split("_beginInUnderscore");
+			Assert.IsTrue(parts.Length == 3);
+		}
+
+
+		[Test]
+		public void TestPerformance()
+		{
+			var watch = new Stopwatch();
+			watch.Start();
+			for(int i = 0; i < 500; i++)
+			{
+				string[] parts = WordSplitter.split("_beginInUnderscore");
+				Assert.IsTrue(parts.Length == 3);
+			}
+			watch.Stop();
+			Assert.IsTrue(watch.ElapsedMilliseconds<500);
 		}
 
 	}
