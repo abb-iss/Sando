84d5fc9fd0a28fe9ffed8fb7543bfae9aca88e23
lordlothar <devnull@localhost>
2012-02-15 22:58:52 +0100

    Add: SolutionKey class added

---
+++ Core/Core/ISolutionKey.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sando.Core
+{
+	public interface ISolutionKey
+	{
+		string GetKeyRepresentation();
+		string GetSolutionPath();
+	}
+}
---
+++ Core/Core/SolutionKey.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace Sando.Core
+{
+	public class SolutionKey : ISolutionKey
+	{
+		public SolutionKey(string solutionPath, string luceneDirectoryPath)
+		{
+			Contract.Requires(!String.IsNullOrWhiteSpace(solutionPath), "SolutionKey:Constructor - solution path cannot be null or an empty string!");
+			Contract.Requires(!String.IsNullOrWhiteSpace(luceneDirectoryPath), "SolutionKey:Constructor - lucene directory path cannot be null or an empty string!");
+
+			this.solutionPath = solutionPath;
+			this.luceneDirectoryPath = luceneDirectoryPath;
+		}
+		
+		public string GetKeyRepresentation()
+		{
+			return luceneDirectoryPath;
+		}
+
+		public string GetSolutionPath()
+		{
+			return solutionPath;
+		}
+
+		private string solutionPath;
+		private string luceneDirectoryPath;
+	}
+}
