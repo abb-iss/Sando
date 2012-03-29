b18a653251d60f5e7df0f2c9b8d2c855de3cb924
lordlothar <devnull@localhost>
2012-03-30 00:01:07 +0200

    Add: First all elements integration tests added

---
+++ Sando/IntegrationTests/Search/AllElementSearchTest.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
+using Sando.Core;
+using Sando.Indexer;
+using Sando.Indexer.Searching;
+using Sando.Indexer.Searching.Criteria;
+using Sando.SearchEngine;
+using Sando.UI.Monitoring;
+
+namespace Sando.IntegrationTests.Search
+{
+	[TestFixture]
+	public class AllElementsSearchTest
+	{
+		[Test]
+		public void SearchRespectsAccessLevelCriteria()
+		{
+			var codeSearcher = new CodeSearcher(IndexerSearcherFactory.CreateSearcher(key));
+			string keywords = "usage type";
+			SearchCriteria searchCriteria = new SimpleSearchCriteria()
+			{
+				AccessLevels = new SortedSet<AccessLevel>() { AccessLevel.Private },
+				SearchByAccessLevel = true,
+				SearchTerms = new SortedSet<string>(keywords.Split(' '))
+			};
+			List<CodeSearchResult> codeSearchResults = codeSearcher.Search(searchCriteria);
+			Assert.AreEqual(codeSearchResults.Count, 2, "Invalid results number");
+			var methodSearchResult = codeSearchResults.Find(el =>
+																el.Element.ProgramElementType == ProgramElementType.Method &&
+																(el.Element.Name == "UsageTypeCriteriaToString"));
+			if(methodSearchResult == null)
+			{
+				Assert.Fail("Failed to find relevant search result for search: " + keywords);
+			}
+			var method = methodSearchResult.Element as MethodElement;
+			Assert.AreEqual(method.AccessLevel, AccessLevel.Private, "Method access level differs!");
+			Assert.AreEqual(method.Arguments, "StringBuilder stringBuilder bool searchByUsageType", "Method arguments differs!");
+			Assert.NotNull(method.Body, "Method body is null!");
+			//Assert.True(method.ClassId != null && method.ClassId != Guid.Empty, "Class id is invalid!");
+			//Assert.AreEqual(method.ClassName, "SimpleSearchCriteria", "Method class name differs!");
+			Assert.AreEqual(method.DefinitionLineNumber, 96, "Method definition line number differs!");
+			Assert.True(method.FullFilePath.EndsWith("\\TestFiles\\MethodElementTestFiles\\Searcher.cs"), "Method full file path is invalid!");
+			Assert.AreEqual(method.Name, "UsageTypeCriteriaToString", "Method name differs!");
+			Assert.AreEqual(method.ProgramElementType, ProgramElementType.Method, "Program element type differs!");
+			Assert.AreEqual(method.ReturnType, "void", "Method return type differs!");
+			Assert.False(String.IsNullOrWhiteSpace(method.Snippet), "Method snippet is invalid!");
+
+			methodSearchResult = codeSearchResults.Find(el =>
+															el.Element.ProgramElementType == ProgramElementType.Method &&
+															(el.Element.Name == "SingleUsageTypeCriteriaToString"));
+			if(methodSearchResult == null)
+			{
+				Assert.Fail("Failed to find relevant search result for search: " + keywords);
+			}
+			method = methodSearchResult.Element as MethodElement;
+			Assert.AreEqual(method.AccessLevel, AccessLevel.Private, "Method access level differs!");
+			Assert.AreEqual(method.Arguments, "StringBuilder stringBuilder UsageType usageType", "Method arguments differs!");
+			Assert.NotNull(method.Body, "Method body is null!");
+			//Assert.True(method.ClassId != null && method.ClassId != Guid.Empty, "Class id is invalid!");
+			//Assert.AreEqual(method.ClassName, "SimpleSearchCriteria", "Method class name differs!");
+			Assert.AreEqual(method.DefinitionLineNumber, 141, "Method definition line number differs!");
+			Assert.True(method.FullFilePath.EndsWith("\\TestFiles\\MethodElementTestFiles\\Searcher.cs"), "Method full file path is invalid!");
+			Assert.AreEqual(method.Name, "SingleUsageTypeCriteriaToString", "Method name differs!");
+			Assert.AreEqual(method.ProgramElementType, ProgramElementType.Method, "Program element type differs!");
+			Assert.AreEqual(method.ReturnType, "void", "Method return type differs!");
+			Assert.False(String.IsNullOrWhiteSpace(method.Snippet), "Method snippet is invalid!");
+		}
+
+		[SetUp]
+		public void Setup()
+		{
+			indexPath = Path.Combine(Path.GetTempPath(), "MethodElementSearchTest");
+			Directory.CreateDirectory(indexPath);
+			key = new SolutionKey(new Guid(), "..\\..\\TestFiles\\MethodElementTestFiles", indexPath);
+			var indexer = DocumentIndexerFactory.CreateIndexer(key, AnalyzerType.Snowball);
+			monitor = new SolutionMonitor(new SolutionWrapper(), key, indexer);
+			string[] files = Directory.GetFiles("..\\..\\TestFiles\\MethodElementTestFiles");
+			foreach(var file in files)
+			{
+				string fullPath = Path.GetFullPath(file);
+				monitor.ProcessFileForTesting(fullPath);
+			}
+			monitor.UpdateAfterAdditions();
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			monitor.StopMonitoring();
+			Directory.Delete(indexPath, true);
+		}
+
+		private string indexPath;
+		private static SolutionMonitor monitor;
+		private static SolutionKey key;
+	}
+}
