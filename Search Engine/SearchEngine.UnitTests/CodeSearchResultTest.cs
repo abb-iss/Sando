c49e672b2a33226aa91d5008e132caf005aacf76
davidcshepherd <devnull@localhost>
2012-03-30 14:16:32 -0400

    Small UI-related updates.

---
+++ Search Engine/SearchEngine.UnitTests/CodeSearchResultTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Sando.SearchEngine.UnitTests
+{
+    [TestFixture]
+    public class CodeSearchResultTest
+    {
+        [TestCase]
+        public void FixSnipTest()
+        {
+            var stuff = "	public void yo()\n		sasdfsadf\n		asdfasdf";
+            Assert.IsTrue(CodeSearchResult.FixSnip(stuff).Equals("public void yo()\n	sasdfsadf\n	asdfasdf"));
+        }
+    }
+}
