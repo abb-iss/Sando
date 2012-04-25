0bfd92c5a373ef5aef3ac0615011cbc0204eb1a8
lordlothar <devnull@localhost>
2012-04-25 21:42:02 +0200

    Change: Disabling BackgroundWorkersManagerTest - unused

--- UI/UI.UnitTests/Monitoring/BackgroundWorkersManagerTest.cs
+++ UI/UI.UnitTests/Monitoring/BackgroundWorkersManagerTest.cs
@@ -12,31 +12,31 @@ namespace Sando.UI.UnitTests.Monitoring
 	[TestFixture]
 	public class BackgroundWorkersManagerTest
 	{
-		[Test]
-		public void BackgroundWorkersManager_RunsOnlyGivenNumberOfWorkersAtOneTime()
-		{
-			int totalBackgroundWorkers = 11;
-			int maxNrOfParallelWorkers = 5;
+		//[Test]
+		//public void BackgroundWorkersManager_RunsOnlyGivenNumberOfWorkersAtOneTime()
+		//{
+		//    int totalBackgroundWorkers = 11;
+		//    int maxNrOfParallelWorkers = 5;
 
-			BackgroundWorker queueEmptyEventWorker = new BackgroundWorker();
-			queueEmptyEventWorker.DoWork += new DoWorkEventHandler(queueEmptyEventWorker_DoWork);
-			BackgroundWorkersManager<int> backgroundWorkersManager = new BackgroundWorkersManager<int>(maxNrOfParallelWorkers, queueEmptyEventWorker);
-			for(int i = 0; i < totalBackgroundWorkers; ++i)
-			{
-				BackgroundWorker worker = new BackgroundWorker();
-				worker.DoWork += new DoWorkEventHandler(worker_DoWork);
-				backgroundWorkersManager.AddWorker(worker, i);
-                Thread.Sleep(100);
-			}
-			Assert.True(backgroundWorkersManager.GetNumberOfCurrentlyRunningWorkers() <= maxNrOfParallelWorkers, "BackgroundWorkersManager currently running workers number too high!");
-			while(counter < totalBackgroundWorkers)
-			{
-			}
-			Thread.Sleep(1000);
-			Assert.True(backgroundWorkersManager.GetNumberOfCurrentlyRunningWorkers() == 0, "BackgroundWorkersManager currently running workers does not equal 0!");
-			if(!allElementsProcessed)
-				Assert.Fail("queueEmptyEventWorker have not been called!");
-		}
+		//    BackgroundWorker queueEmptyEventWorker = new BackgroundWorker();
+		//    queueEmptyEventWorker.DoWork += new DoWorkEventHandler(queueEmptyEventWorker_DoWork);
+		//    BackgroundWorkersManager<int> backgroundWorkersManager = new BackgroundWorkersManager<int>(maxNrOfParallelWorkers, queueEmptyEventWorker);
+		//    for(int i = 0; i < totalBackgroundWorkers; ++i)
+		//    {
+		//        BackgroundWorker worker = new BackgroundWorker();
+		//        worker.DoWork += new DoWorkEventHandler(worker_DoWork);
+		//        backgroundWorkersManager.AddWorker(worker, i);
+		//        Thread.Sleep(100);
+		//    }
+		//    Assert.True(backgroundWorkersManager.GetNumberOfCurrentlyRunningWorkers() <= maxNrOfParallelWorkers, "BackgroundWorkersManager currently running workers number too high!");
+		//    while(counter < totalBackgroundWorkers)
+		//    {
+		//    }
+		//    Thread.Sleep(1000);
+		//    Assert.True(backgroundWorkersManager.GetNumberOfCurrentlyRunningWorkers() == 0, "BackgroundWorkersManager currently running workers does not equal 0!");
+		//    if(!allElementsProcessed)
+		//        Assert.Fail("queueEmptyEventWorker have not been called!");
+		//}
 
 		void queueEmptyEventWorker_DoWork(object sender, DoWorkEventArgs e)
 		{
