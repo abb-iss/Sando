3073eb394a9fb21a330dd9cfb4fbe630d048936c
kostata <devnull@localhost>
2012-05-03 12:49:47 -0400

    Renamed ParserExtensions to ExperimentalExtensions

---
+++ ExperimentalExtensions/TextFileParserExtension/TextFileElement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sando.ExtensionContracts.ProgramElementContracts;
+
+namespace Sando.ParserExtensions
+{
+    public class TextFileElement : ProgramElement
+    {
+        public TextFileElement(string name, int definitionLineNumber, string fullFilePath, string snippet) : base(name, definitionLineNumber, fullFilePath, snippet)
+        {
+        }
+
+        public String Body { get; set; }
+
+        public override ProgramElementType ProgramElementType
+        {
+            get { return ProgramElementType.Custom; }
+        }
+
+        public override string GetName()
+        {
+            return "Text File";
+        }
+    }
+}
