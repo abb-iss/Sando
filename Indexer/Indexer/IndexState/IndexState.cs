572d050b5388777fce98451ebd03c4916fe5105b
lordlothar <devnull@localhost>
2012-05-10 22:40:48 +0200

    Add: IndexState and IndexStateManager classes added - responsible for detecting internal index structure changes

---
+++ Indexer/Indexer/IndexState/IndexState.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Sando.Indexer.IndexState
+{
+	public class IndexState
+	{
+		public IndexState()
+		{
+			UsedFieldNames = new List<string>();
+			UsedParsers = new Dictionary<string, string>();
+		}
+
+		public List<string> UsedFieldNames { get; set; }
+		public Dictionary<string, string> UsedParsers { get; set; }
+		public string UsedSplitter { get; set; }
+		public string UsedAnalyzer { get; set; }
+
+		public override bool Equals(object obj)
+		{
+			IndexState indexState = obj as IndexState;
+			if(indexState == null)
+				return false;
+			
+			if(UsedFieldNames.Count != indexState.UsedFieldNames.Count)
+				return false;
+			if(UsedFieldNames.Count(f => !indexState.UsedFieldNames.Contains(f)) > 0)
+				return false;
+
+			if(UsedParsers.Count != indexState.UsedParsers.Count)
+				return false;
+			if(UsedParsers.Count(f => !indexState.UsedParsers.Contains(f)) > 0)
+				return false;
+
+			if(UsedSplitter != indexState.UsedSplitter)
+				return false;
+
+			if(UsedAnalyzer != indexState.UsedAnalyzer)
+				return false;
+
+			return true;
+		}
+
+		public override int GetHashCode()
+		{
+			return base.GetHashCode();
+		}
+	}
+}
---
+++ Indexer/Indexer/IndexState/IndexStateManager.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Xml.Serialization;
+using System.Reflection;
+
+namespace Sando.Indexer.IndexState
+{
+	public class IndexStateManager
+	{
+		[MethodImpl(MethodImplOptions.Synchronized)]
+		public void SaveIndexState()
+		{
+			if(currentIndexState == null)
+				return;
+
+			XmlSerializer xmlSerializer = null;
+			TextWriter textWriter = null;
+			try
+			{
+				xmlSerializer = new XmlSerializer(typeof(IndexState));
+				textWriter = new StreamWriter(indexStatePath);
+				xmlSerializer.Serialize(textWriter, currentIndexState);
+			}
+			finally
+			{
+				if(textWriter != null)
+					textWriter.Close();
+			}
+		}
+
+		[MethodImpl(MethodImplOptions.Synchronized)]
+		public void SetCurrentIndexState(IndexState currentIndexState)
+		{
+			this.currentIndexState = currentIndexState;
+		}
+
+		[MethodImpl(MethodImplOptions.Synchronized)]
+		public bool IsIndexRecreationRequired()
+		{
+			return previousIndexState.Equals(currentIndexState);
+		}
+
+		public static IndexStateManager Instance
+		{
+			get
+			{
+				if(indexStateManager == null)
+					indexStateManager = new IndexStateManager();
+				
+				return indexStateManager;
+			}
+		}
+
+		private IndexStateManager()
+		{
+			Contract.Requires(!String.IsNullOrWhiteSpace(indexDirectoryPath), "IndexStateManager:Constructor - index directory path cannot be null or an empty string!");
+			Contract.Requires(Directory.Exists(indexDirectoryPath), "IndexStateManager:Constructor - index directory path does not point to a valid directory!");
+
+			this.indexDirectoryPath = Assembly.GetCallingAssembly().Location;
+			this.indexStatePath = Path.Combine(indexDirectoryPath, IndexStateFileName);
+
+			readPreviousIndexState();
+		}
+
+		private void readPreviousIndexState()
+		{
+			previousIndexState = new IndexState();
+			if(!File.Exists(indexStatePath))
+			{
+				return; //no file - solution indexed for the first time
+			}
+
+			XmlSerializer xmlSerializer = null;
+			TextReader textReader = null;
+			try
+			{
+				xmlSerializer = new XmlSerializer(typeof(IndexState));
+				textReader = new StreamReader(indexStatePath);
+				previousIndexState = (IndexState)xmlSerializer.Deserialize(textReader);
+			}
+			finally
+			{
+				if(textReader != null)
+					textReader.Close();
+			}
+		}
+
+		private const string IndexStateFileName = "sandoindexstate.xml";
+
+		private string indexDirectoryPath;
+		private string indexStatePath;
+		private IndexState previousIndexState;
+		private IndexState currentIndexState;
+
+		private static IndexStateManager indexStateManager;
+	}
+}
