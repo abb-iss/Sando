3860227240c220772f4892503c3c33e58b533690
kostata <devnull@localhost>
2012-08-10 14:09:45 -0400

    Added ability to upload sando logs to Amazon S3.

---
+++ Core/Core.UnitTests/Extensions/Logging/S3LogWriterTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Amazon.S3;
+using NUnit.Framework;
+using Sando.Core.Extensions.Logging;
+
+namespace Sando.Core.UnitTests.Extensions.Logging
+{
+	[TestFixture]
+	class S3LogWriterTest
+	{
+		[Test]
+		public void TestWriteLog()
+		{
+			if(!System.IO.File.Exists(S3LogWriter.S3CredentialFileLocation))
+			{
+				//logging is off due to unavailabily of S3 credentials
+				return;
+			}
+			else 
+			{
+				string logFilePath = Environment.CurrentDirectory + "\\..\\..\\Core\\Core.UnitTests\\TestFiles\\TestLog.txt";
+				bool success = S3LogWriter.WriteLogFile(logFilePath);
+				Assert.IsTrue(success);
+			}
+		}
+	}
+}
