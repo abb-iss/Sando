3a1fd6a92b2ee4fb92ead4dd43529253c1c7a9f6
kostata <devnull@localhost>
2012-08-13 14:34:14 -0400

    Cleaned up the balanced interleaving stuff

--- Core/Core.UnitTests/Extensions/PairedInterleaving/PairedInterleavingTest.cs
+++ Core/Core.UnitTests/Extensions/PairedInterleaving/PairedInterleavingTest.cs
@@ -12,52 +12,43 @@ namespace Sando.Core.UnitTests.Extensions.PairedInterleaving
     [TestFixture]
     class PairedInterleavingTest
     {
-        private PairedInterleavingManager manager;
-        private List<CodeSearchResult> sandoResults;
-        private List<CodeSearchResult> lexResults;
-        private List<CodeSearchResult> interleavedResults;
-        private List<int> clickIdx;
+        private List<CodeSearchResult> _sandoResults;
+        private List<CodeSearchResult> _lexResults;
+        private List<CodeSearchResult> _interleavedResults;
+        private List<int> _clickIdx;
 
         [TestFixtureSetUp]
         public void SetupExperiment()
         {
-            CodeSearchResult a = new CodeSearchResult(new CommentElement("a", 1, "abcd.cs", "a", "a"), 1);
-            CodeSearchResult b = new CodeSearchResult(new CommentElement("b", 1, "abcd.cs", "b", "b"), 1);
-            CodeSearchResult c = new CodeSearchResult(new CommentElement("c", 1, "abcd.cs", "c", "c"), 1);
-            CodeSearchResult d = new CodeSearchResult(new CommentElement("d", 1, "abcd.cs", "d", "d"), 1);
+            var a = new CodeSearchResult(new CommentElement("a", 1, "abcd.cs", "a", "a"), 1);
+            var b = new CodeSearchResult(new CommentElement("b", 1, "abcd.cs", "b", "b"), 1);
+            var c = new CodeSearchResult(new CommentElement("c", 1, "abcd.cs", "c", "c"), 1);
+            var d = new CodeSearchResult(new CommentElement("d", 1, "abcd.cs", "d", "d"), 1);
 
-            sandoResults = new List<CodeSearchResult>();
-            sandoResults.Add(a); sandoResults.Add(b); sandoResults.Add(c);
-
-            lexResults = new List<CodeSearchResult>();
-            lexResults.Add(b); lexResults.Add(d); lexResults.Add(c);
-            
-            manager = new PairedInterleavingManager();
-            interleavedResults = manager.BalancedInterleave(sandoResults, lexResults);
-
-            clickIdx = new List<int>();
-            clickIdx.Add(interleavedResults.IndexOf(b));
-            clickIdx.Add(interleavedResults.IndexOf(c));
+            _sandoResults = new List<CodeSearchResult> {a, b, c};
+        	_lexResults = new List<CodeSearchResult> {b, d, c};
+            _interleavedResults = BalancedInterleaving.Interleave(_sandoResults, _lexResults); 
+            _clickIdx = new List<int> {_interleavedResults.IndexOf(b), _interleavedResults.IndexOf(c)};
         }
 
         [Test]
         public void TestBalancedInterleaving()
         {
-            List<CodeSearchResult> interleaving = manager.BalancedInterleave(sandoResults, lexResults);
+            List<CodeSearchResult> interleaving = BalancedInterleaving.Interleave(_sandoResults, _lexResults);
             Assert.AreEqual(interleaving.Count, 4);
-            if (interleaving[0] == sandoResults[0])
+            if (interleaving[0] == _sandoResults[0])
             {
                 //b,d,c
-                Assert.IsTrue(interleaving[1] == lexResults[0]);
-                Assert.IsTrue(interleaving[2] == lexResults[1]);
-                Assert.IsTrue(interleaving[3] == sandoResults[2]);
+                Assert.IsTrue(interleaving[1] == _lexResults[0]);
+                Assert.IsTrue(interleaving[2] == _lexResults[1]);
+                Assert.IsTrue(interleaving[3] == _sandoResults[2]);
             }
-            else if (interleaving[0] == lexResults[0])
+            else if (interleaving[0] == _lexResults[0])
             {
                 //a,d,c
-                Assert.IsTrue(interleaving[1] == sandoResults[0]);
-                Assert.IsTrue(interleaving[2] == lexResults[1]);
-                Assert.IsTrue(interleaving[3] == sandoResults[2]);
+                Assert.IsTrue(interleaving[1] == _sandoResults[0]);
+                Assert.IsTrue(interleaving[2] == _lexResults[1]);
+                Assert.IsTrue(interleaving[3] == _sandoResults[2]);
             }
             else
             {
@@ -69,8 +60,8 @@ namespace Sando.Core.UnitTests.Extensions.PairedInterleaving
         public void TestDetermineWinner()
         {
             int scoreA, scoreB;
-            manager.DetermineWinner(sandoResults, lexResults, interleavedResults, 
-                                        clickIdx, out scoreA, out scoreB);
+            BalancedInterleaving.DetermineWinner(_sandoResults, _lexResults, _interleavedResults, 
+                                        _clickIdx, out scoreA, out scoreB);
             Assert.AreEqual(scoreA, 0);
             Assert.AreEqual(scoreB, 1);
         }
--- Core/Core/Extensions/PairedInterleaving/PairedInterleavingManager.cs
+++ Core/Core/Extensions/PairedInterleaving/PairedInterleavingManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Sando.Core.Extensions.Logging;
 using Sando.ExtensionContracts.QueryContracts;
 using Sando.ExtensionContracts.ResultsReordererContracts;
 
@@ -13,26 +14,33 @@ namespace Sando.Core.Extensions.PairedInterleaving
 		{
 			LogCount = 0;
             ClickIdx = new List<int>();
-            IsLogEntryReady = false;
+            searchRecievedClick = false;
+			InitializeLogFileName();
 		}
 
 		public string RewriteQuery(string query)
 		{
-			//write a log entry for the previous query (now that the clicking has completed for it)
-            if (IsLogEntryReady)
+			//dump the previous query stuff to the log, assuming it was clicked
+            if (searchRecievedClick)
             {
                 LogCount++;
                 int scoreA, scoreB;
-                DetermineWinner(SandoResults, SecondaryResults, InterleavedResults, ClickIdx, out scoreA, out scoreB);
-                //write to file...
+                BalancedInterleaving.DetermineWinner(SandoResults, SecondaryResults, InterleavedResults, 
+														ClickIdx, out scoreA, out scoreB);
+
+            	string entry = LogCount + ": " + FLT_A_NAME + "=" + scoreA + ", " + FLT_B_NAME + "=" + scoreB + Environment.NewLine;
+				WriteLogEntry(LogFile, entry);
             }
 
-			//capture the query and reissue it to the secondary FLT getting the secondary results
+			//TODO: capture the query and reissue it to the secondary FLT getting the secondary results
+			SecondaryResults = SandoResults;
+			SecondaryResults.Reverse();
 
-			//check the number of log entires collected and decide whether to push the log to S3
+			//write log to S3
             if (LogCount >= LOG_ENTRIES_PER_FILE)
             {
-                //...
+            	S3LogWriter.WriteLogFile(LogFile);
+				InitializeLogFileName();
             }
 
             return query;
@@ -41,7 +49,7 @@ namespace Sando.Core.Extensions.PairedInterleaving
 		public IQueryable<CodeSearchResult> ReorderSearchResults(IQueryable<CodeSearchResult> searchResults)
 		{
             SandoResults = searchResults.ToList();
-            InterleavedResults = BalancedInterleave(searchResults.ToList(), SecondaryResults);
+            InterleavedResults = BalancedInterleaving.Interleave(searchResults.ToList(), SecondaryResults);
             return InterleavedResults.AsQueryable(); 
         }
 
@@ -51,81 +59,28 @@ namespace Sando.Core.Extensions.PairedInterleaving
             if (InterleavedResults != null && InterleavedResults.Count > 0)
             {
                 ClickIdx.Add(InterleavedResults.IndexOf(clickedElement));
-                IsLogEntryReady = true;
+                searchRecievedClick = true;
             }
 		}
 
-        public List<CodeSearchResult> BalancedInterleave(List<CodeSearchResult> A, List<CodeSearchResult> B)
-        {
-            List<CodeSearchResult> I = new List<CodeSearchResult>();
-            int Ka = 0, Kb = 0;
+		private void WriteLogEntry(string filename, string entry)
+		{
+			System.IO.File.AppendAllText(filename, entry);
+		}
 
-            Random random = new Random();
-            bool AFirst = (random.NextDouble() >= 0.5);
+		private void InitializeLogFileName()
+		{
+			LogFile = Environment.CurrentDirectory + "\\PairedInterleaving-" + Environment.MachineName + "-" + Guid.NewGuid() + ".dat";
+		}
 
-            while (Ka < A.Count && Kb < B.Count)
-            {
-                if (Ka < Kb || (Ka == Kb && AFirst == true))
-                {
-                    if (!I.Contains(A[Ka]))
-                    {
-                        I.Add(A[Ka]);
-                    }
-                    Ka++;
-                }
-                else
-                {
-                    if (!I.Contains(B[Kb]))
-                    {
-                        I.Add(B[Kb]);
-                    }
-                    Kb++;
-                }
-            }
-
-            return I;
-        }
-
-        public void DetermineWinner(List<CodeSearchResult> A, List<CodeSearchResult> B, List<CodeSearchResult> I, 
-                                    List<int> C, out int scoreA, out int scoreB)
-        {
-            scoreA = 0; 
-            scoreB = 0;
-
-            if (C.Count <= 0) return;
-
-            int Cmax = C.Max();
-            int Pa = A.IndexOf(I[Cmax]);
-            int Pb = B.IndexOf(I[Cmax]);
-            if (Pa == -1) Pa = Int32.MaxValue;
-            if (Pb == -1) Pb = Int32.MaxValue;
-            int K = Math.Min(Pa, Pb);
-
-            foreach (int c in C)
-            {
-                int Ha = A.IndexOf(I[c]);
-                int Hb = B.IndexOf(I[c]);
-                if (Ha == -1) Ha = Int32.MaxValue;
-                if (Hb == -1) Hb = Int32.MaxValue;
-
-                if (Ha < Hb)
-                {
-                    scoreA++;
-                }
-                else if (Hb < Ha)
-                {
-                    scoreB++;
-                }
-            }
-        }
-
-        private readonly int LOG_ENTRIES_PER_FILE = 50;
-        private readonly string FLT_A_NAME = "Sando";
-        private readonly string FLT_B_NAME = "Lex";
+		private const int LOG_ENTRIES_PER_FILE = 50;
+		private const string FLT_A_NAME = "Sando";
+        private const string FLT_B_NAME = "Lex";
+		private string LogFile;
 
         private List<CodeSearchResult> SecondaryResults;
         private List<CodeSearchResult> SandoResults;
-        private bool IsLogEntryReady;
+        private bool searchRecievedClick;
 
         public List<CodeSearchResult> InterleavedResults { get; private set; }
         public List<int> ClickIdx { get; private set; }
