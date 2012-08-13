448fdf661197ffdb13a697f5adde5fe61720fbfd
kostata <devnull@localhost>
2012-08-12 22:38:19 -0400

    Added balanced interleaving to paired eval

---
+++ Core/Core.UnitTests/Extensions/PairedInterleaving/PairedInterleavingTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Sando.Core.Extensions.PairedInterleaving;
+using Sando.ExtensionContracts.ResultsReordererContracts;
+using Sando.ExtensionContracts.ProgramElementContracts;
+
+namespace Sando.Core.UnitTests.Extensions.PairedInterleaving
+{
+    [TestFixture]
+    class PairedInterleavingTest
+    {
+        private PairedInterleavingManager manager;
+        private List<CodeSearchResult> sandoResults;
+        private List<CodeSearchResult> lexResults;
+        private List<CodeSearchResult> interleavedResults;
+        private List<int> clickIdx;
+
+        [TestFixtureSetUp]
+        public void SetupExperiment()
+        {
+            CodeSearchResult a = new CodeSearchResult(new CommentElement("a", 1, "abcd.cs", "a", "a"), 1);
+            CodeSearchResult b = new CodeSearchResult(new CommentElement("b", 1, "abcd.cs", "b", "b"), 1);
+            CodeSearchResult c = new CodeSearchResult(new CommentElement("c", 1, "abcd.cs", "c", "c"), 1);
+            CodeSearchResult d = new CodeSearchResult(new CommentElement("d", 1, "abcd.cs", "d", "d"), 1);
+
+            sandoResults = new List<CodeSearchResult>();
+            sandoResults.Add(a); sandoResults.Add(b); sandoResults.Add(c);
+
+            lexResults = new List<CodeSearchResult>();
+            lexResults.Add(b); lexResults.Add(d); lexResults.Add(c);
+            
+            manager = new PairedInterleavingManager();
+            interleavedResults = manager.BalancedInterleave(sandoResults, lexResults);
+
+            clickIdx = new List<int>();
+            clickIdx.Add(interleavedResults.IndexOf(b));
+            clickIdx.Add(interleavedResults.IndexOf(c));
+        }
+
+        [Test]
+        public void TestBalancedInterleaving()
+        {
+            List<CodeSearchResult> interleaving = manager.BalancedInterleave(sandoResults, lexResults);
+            Assert.AreEqual(interleaving.Count, 4);
+            if (interleaving[0] == sandoResults[0])
+            {
+                //b,d,c
+                Assert.IsTrue(interleaving[1] == lexResults[0]);
+                Assert.IsTrue(interleaving[2] == lexResults[1]);
+                Assert.IsTrue(interleaving[3] == sandoResults[2]);
+            }
+            else if (interleaving[0] == lexResults[0])
+            {
+                //a,d,c
+                Assert.IsTrue(interleaving[1] == sandoResults[0]);
+                Assert.IsTrue(interleaving[2] == lexResults[1]);
+                Assert.IsTrue(interleaving[3] == sandoResults[2]);
+            }
+            else
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public void TestDetermineWinner()
+        {
+            int scoreA, scoreB;
+            manager.DetermineWinner(sandoResults, lexResults, interleavedResults, 
+                                        clickIdx, out scoreA, out scoreB);
+            Assert.AreEqual(scoreA, 0);
+            Assert.AreEqual(scoreB, 1);
+        }
+
+
+    }
+}
--- Core/Core/Extensions/PairedInterleaving/PairedInterleavingManager.cs
+++ Core/Core/Extensions/PairedInterleaving/PairedInterleavingManager.cs
@@ -7,48 +7,128 @@ using Sando.ExtensionContracts.ResultsReordererContracts;
 
 namespace Sando.Core.Extensions.PairedInterleaving
 {
-	class PairedInterleavingManager : IQueryRewriter, IResultsReorderer
+	public class PairedInterleavingManager : IQueryRewriter, IResultsReorderer
 	{
-		private List<CodeSearchResult> SecondaryResults;
-		private PairedInterleavingLogEntry LogEntry;
-		private int LogCount;
-
 		public PairedInterleavingManager()
 		{
 			LogCount = 0;
+            ClickIdx = new List<int>();
+            IsLogEntryReady = false;
 		}
 
 		public string RewriteQuery(string query)
 		{
 			//write a log entry for the previous query (now that the clicking has completed for it)
+            if (IsLogEntryReady)
+            {
+                LogCount++;
+                int scoreA, scoreB;
+                DetermineWinner(SandoResults, SecondaryResults, InterleavedResults, ClickIdx, out scoreA, out scoreB);
+                //write to file...
+            }
 
 			//capture the query and reissue it to the secondary FLT getting the secondary results
 
 			//check the number of log entires collected and decide whether to push the log to S3
+            if (LogCount >= LOG_ENTRIES_PER_FILE)
+            {
+                //...
+            }
 
-			
-
-			throw new NotImplementedException();
+            return query;
 		}
 
 		public IQueryable<CodeSearchResult> ReorderSearchResults(IQueryable<CodeSearchResult> searchResults)
 		{
-			//interleave the search results from the primary and secondary FLT
-
-			//create a new log entry
-			LogCount++;
-			LogEntry = new PairedInterleavingLogEntry(LogCount, searchResults.ToList());
-
-			throw new NotImplementedException();
-		}
+            SandoResults = searchResults.ToList();
+            InterleavedResults = BalancedInterleave(searchResults.ToList(), SecondaryResults);
+            return InterleavedResults.AsQueryable(); 
+        }
 
 		//called from UI.FileOpener
 		public void NotifyClicked(CodeSearchResult clickedElement)
 		{
-			//determine which FLT this element belongs to and maintain a count 
-			LogEntry.CountClick(clickedElement);
-
-			throw new NotImplementedException();
+            if (InterleavedResults != null && InterleavedResults.Count > 0)
+            {
+                ClickIdx.Add(InterleavedResults.IndexOf(clickedElement));
+                IsLogEntryReady = true;
+            }
 		}
+
+        public List<CodeSearchResult> BalancedInterleave(List<CodeSearchResult> A, List<CodeSearchResult> B)
+        {
+            List<CodeSearchResult> I = new List<CodeSearchResult>();
+            int Ka = 0, Kb = 0;
+
+            Random random = new Random();
+            bool AFirst = (random.NextDouble() >= 0.5);
+
+            while (Ka < A.Count && Kb < B.Count)
+            {
+                if (Ka < Kb || (Ka == Kb && AFirst == true))
+                {
+                    if (!I.Contains(A[Ka]))
+                    {
+                        I.Add(A[Ka]);
+                    }
+                    Ka++;
+                }
+                else
+                {
+                    if (!I.Contains(B[Kb]))
+                    {
+                        I.Add(B[Kb]);
+                    }
+                    Kb++;
+                }
+            }
+
+            return I;
+        }
+
+        public void DetermineWinner(List<CodeSearchResult> A, List<CodeSearchResult> B, List<CodeSearchResult> I, 
+                                    List<int> C, out int scoreA, out int scoreB)
+        {
+            scoreA = 0; 
+            scoreB = 0;
+
+            if (C.Count <= 0) return;
+
+            int Cmax = C.Max();
+            int Pa = A.IndexOf(I[Cmax]);
+            int Pb = B.IndexOf(I[Cmax]);
+            if (Pa == -1) Pa = Int32.MaxValue;
+            if (Pb == -1) Pb = Int32.MaxValue;
+            int K = Math.Min(Pa, Pb);
+
+            foreach (int c in C)
+            {
+                int Ha = A.IndexOf(I[c]);
+                int Hb = B.IndexOf(I[c]);
+                if (Ha == -1) Ha = Int32.MaxValue;
+                if (Hb == -1) Hb = Int32.MaxValue;
+
+                if (Ha < Hb)
+                {
+                    scoreA++;
+                }
+                else if (Hb < Ha)
+                {
+                    scoreB++;
+                }
+            }
+        }
+
+        private readonly int LOG_ENTRIES_PER_FILE = 50;
+        private readonly string FLT_A_NAME = "Sando";
+        private readonly string FLT_B_NAME = "Lex";
+
+        private List<CodeSearchResult> SecondaryResults;
+        private List<CodeSearchResult> SandoResults;
+        private bool IsLogEntryReady;
+
+        public List<CodeSearchResult> InterleavedResults { get; private set; }
+        public List<int> ClickIdx { get; private set; }
+        public int LogCount { get; private set; }
 	}
 }
