7d9b943760217c43925503f049bba5a90aefa415
sysnet <sysnet@Ayodele-HP>
2012-10-15 11:01:05 -0400

    Good amount of progress, but now a weird error in manifest file.

--- LIBS/Contracts/Languages/CSharp/ContractDeclarativeAssemblyAttribute.cs
+++ LIBS/Contracts/Languages/CSharp/ContractDeclarativeAssemblyAttribute.cs
@@ -2,15 +2,12 @@
 //  This file is included when building a contract declarative assembly
 //  in order to mark it as such for recognition by the tools
 //
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 [assembly: global::System.Diagnostics.Contracts.ContractDeclarativeAssembly]
 
 namespace System.Diagnostics.Contracts
 {
-  [AttributeUsage(AttributeTargets.Assembly)]
+  [AttributeUsage(global::System.AttributeTargets.Assembly)]
   internal sealed class ContractDeclarativeAssemblyAttribute : global::System.Attribute
   {
   }
