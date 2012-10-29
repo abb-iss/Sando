3b74112d1269c830a67fc6d4aa59788c1b215e5a
Administrator <Administrator@nhum2s-01.vsu.edu>
2012-10-29 11:53:42 -0400

    Add AWS sdk into VSIX

---
+++ IntegrationTests/Search/InterleavingSearchTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
+using Sando.Core;
+using Sando.ExtensionContracts.ProgramElementContracts;
+using Sando.ExtensionContracts.ResultsReordererContracts;
+using Sando.Indexer;
+using Sando.Indexer.Searching;
+using Sando.Indexer.Searching.Criteria;
+using Sando.SearchEngine;
+using Sando.UI.Monitoring;
+using UnitTestHelpers;
+using Sando.UI.InterleavingExperiment;
+
+namespace Sando.IntegrationTests.Search
+{
+	[TestFixture]
+	public class InterleavingSearchTest
+	{
+	    [Test]
+        public void TestPairedIndexing()
+        {
+
+        }
+
+        [Test]
+        public void TestInterleavedResults()
+        {
+
+        }
+
+        [Test]
+        public void TestClickRecording()
+        {
+
+        }
+
+
+		[TestFixtureSetUp]
+		public void SetUp()
+		{
+			TestUtils.InitializeDefaultExtensionPoints();
+            InterleavingExperimentManager.Instance.InitializeExperimentParticipants(indexPath);
+		}
+
+		[SetUp]
+		public void Setup()
+		{
+			indexPath = Path.Combine(Path.GetTempPath(), "InterleavingSearchTest");
+			Directory.CreateDirectory(indexPath);
+			key = new SolutionKey(Guid.NewGuid(), "..\\..", indexPath);
+			var indexer = DocumentIndexerFactory.CreateIndexer(key, AnalyzerType.Snowball);
+			monitor = new SolutionMonitor(new SolutionWrapper(), key, indexer, false);
+
+			string startingPath = "..\\..";
+			string[] dirs = Directory.GetDirectories(startingPath);
+			ProcessDirectoryForTesting(dirs);
+
+			monitor.UpdateAfterAdditions();
+		}
+
+		private void ProcessDirectoryForTesting(string[] dirs)
+		{
+			foreach (var dir in dirs)
+			{
+				string[] subdirs = Directory.GetDirectories(dir);
+				ProcessDirectoryForTesting(subdirs);
+
+				string[] files = Directory.GetFiles(dir);
+				foreach (var file in files)
+				{ 
+					string fullPath = Path.GetFullPath(file);
+					if (Path.GetExtension(fullPath) == ".cs")
+					{
+						monitor.ProcessFileForTesting(fullPath);
+					}
+				}
+			}
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			monitor.StopMonitoring(true);
+			Directory.Delete(indexPath, true);
+		}
+
+		private string indexPath;
+		private static SolutionMonitor monitor;
+		private static SolutionKey key;
+		private static List<string> sandoDirsToAvoid;
+	}
+}
