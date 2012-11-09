c32de968eb0a94d3f0f1991f443f4c792ff8325e
davidcshepherd <devnull@localhost>
2012-11-09 16:34:25 -0500

    fse demo...

---
+++ Sando/XpandQueriesFSEDemo/QueryExpander.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sando.ExtensionContracts.QueryContracts;
+
+namespace XpandQueriesFSEDemo
+{
+    public class QueryExpander : IQueryRewriter
+    {
+        public string RewriteQuery(string query)
+        {
+            //expand common abbreviations
+            if(String.IsNullOrEmpty(query))
+            {
+                return query;
+            }
+            else
+            {
+                return query.Replace("calc", "calculate");
+            }
+        }
+    }
+}
