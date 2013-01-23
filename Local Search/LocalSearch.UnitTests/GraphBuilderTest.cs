55234e9b27916eae86565e306548bb63aca7964e
qxshower2013 <devnull@localhost>
2013-01-23 10:27:50 -0500

    Getting basic information of field and method

---
+++ Local Search/LocalSearch.UnitTests/GraphBuilderTest.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ABB.SrcML;
+using NUnit.Framework;
+using System.Xml.Linq;
+
+namespace LocalSearch.UnitTests
+{
+    [TestFixture]
+    public class GraphBuilderTest
+    {
+        private static String srcPath = @"C:\temp\ConfigManip.cs";
+        private static String xmlPath = @"C:\temp\ConfigManip.XML";
+
+        private static String singlemethodPath = @"C:\temp\TestMethod.cs";
+        
+        //[Test]
+        //public void ConvertSrcToXMLTest()
+        //{
+        //    Src2SrcMLRunner srcmlConverter;
+        //    String fileExt = Path.GetExtension(srcPath);
+        //    if(fileExt.Equals(".cs"))
+        //        srcmlConverter = new Src2SrcMLRunner(@"C:\WORK-XIAO\sando\LIBS\srcML-Win-cSharp");
+        //    else
+        //        srcmlConverter = new Src2SrcMLRunner(@"C:\WORK-XIAO\sando\LIBS\srcML-Win");
+            
+        //    var tempSrcMLFile = srcmlConverter.GenerateSrcMLFromFile(srcPath, xmlPath);            
+        //}
+
+        [Test]
+        public void GetFieldNamesTest()
+        {
+            List<String> FieldNames = new List<String>() { "configWorkspaceRoot", 
+                "configList", "configSelect", "files_in_build", "allSelections", 
+                "configCnt", "fileImpacted", "lineCntImpactedPerFile", "funcImpacted",
+                "lineCntImpactedPerFunc", "code_change_imp_file", "configIndexImpacted", "configImpacted"};
+                        
+            GraphBuilder gbuilder = new GraphBuilder(srcPath);
+            var fields = gbuilder.GetFieldNames();
+
+            foreach (var field in fields)
+            {                
+                String fieldname = field.Value;
+                //Console.WriteLine(fieldname);
+                Assert.IsTrue(FieldNames.Contains(fieldname));
+                FieldNames.Remove(fieldname);
+            }
+
+            Assert.IsEmpty(FieldNames);
+        }
+
+        [Test]
+        public void GetMethodNamesTest()
+        {
+            List<String> MethodNames = new List<string>()
+            { "toUpper", "convertInt", "replaceCharInStr", "removeCharInStr", 
+              "ConfigManip", "ConfigManip", "displayConfig", "getConfigSelect", 
+              "getChangeImpact","genConfigImp","findChangeImpConfig","genConfigImp",
+              "findChangeImpConfig","getConfigImpacted","genSingleConfigImp","genMultiSelectImp",
+              "getChangeSets","getFileInBuild","genFileImp","genFuncImp",
+              "genImpUnionFile","genImpUnionFunc"
+            };
+
+            GraphBuilder gbuilder = new GraphBuilder(xmlPath);
+            var methods = gbuilder.GetMethodNames();
+
+            foreach (var method in methods)
+            {
+                String methodname = method.Value;
+                Assert.IsTrue(MethodNames.Contains(methodname));
+                MethodNames.Remove(methodname);
+            }
+
+            Assert.IsEmpty(MethodNames);
+        }
+
+        [Test]
+        public void GetAllLocalVarsinMethodTest()
+        {
+            List<String> LocalVars = new List<String>() { "", "", "", "" }; 
+                
+            GraphBuilder gbuilder = new GraphBuilder(singlemethodPath);
+            var methods = gbuilder.GetMethods();
+            foreach (var method in methods) //should be only one method
+            {
+                var localvars = gbuilder.GetAllLocalVarsinMethod(method);
+            }
+        }
+    }
+    
+}
---
+++ Local Search/LocalSearch/GraphBuilder.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using ABB.SrcML;
+using System.Runtime.CompilerServices;
+
+namespace LocalSearch
+{   
+    public class GraphBuilder
+    {
+        private const String SrcmlLibSharp = @"C:\WORK-XIAO\sando\LIBS\srcML-Win-cSharp";
+        private const String SrcmlLib = @"C:\WORK-XIAO\sando\LIBS\srcML-Win";
+        private SrcMLFile srcmlFile;
+        private XElement[] FieldDecs;
+        private XElement[] Methods;
+
+        /// <summary>
+        /// Constructing a new Graph Builder.
+        /// </summary>
+        /// <param name="srcPath">The full path to the given Source Code or XML file.</param>
+        public GraphBuilder(String srcPath)
+        {
+            Src2SrcMLRunner srcmlConverter;
+            String fileExt = Path.GetExtension(srcPath);
+
+            if (fileExt.Equals(".xml", StringComparison.CurrentCultureIgnoreCase) )
+                srcmlFile = new SrcMLFile(srcPath);
+            else
+            {
+                if (fileExt.Equals(".cs", StringComparison.CurrentCultureIgnoreCase))
+                    srcmlConverter = new Src2SrcMLRunner(SrcmlLibSharp);
+                else
+                    srcmlConverter = new Src2SrcMLRunner(SrcmlLib);
+
+                String tmpFile = Path.GetTempPath() + Guid.NewGuid().ToString() + ".xml";
+                srcmlFile = srcmlConverter.GenerateSrcMLFromFile(srcPath, tmpFile);
+            }
+        }
+
+        /// <summary>
+        /// Get all field declarations in the source file.
+        /// </summary>
+        /// <returns>An array (of "decl_stmt" XElement) that contains all the field declarations.</returns>
+        public XElement[] GetFieldDecs()
+        {
+            List<XElement> AllFields = new List<XElement>();
+            
+            foreach (XElement file in srcmlFile.FileUnits)
+            {
+                var fields = from field in file.Descendants()
+                                where field.Name.Equals(SRC.DeclarationStatement)
+                                && !field.Ancestors(SRC.Function).Any()
+                                && !field.Ancestors(SRC.Constructor).Any()
+                                && !field.Ancestors(SRC.Destructor).Any()
+                                select field;
+                
+                if (fields != null)
+                    AllFields.AddRange(fields);
+            }
+
+            //FieldDecs = AllFields.ToArray();
+            return AllFields.ToArray();
+        }
+
+        /// <summary>
+        /// Get all field names declared in the source file.
+        /// </summary>
+        /// <returns>An array (of "name" XElement) that contains all the field names.</returns>
+        public XElement[] GetFieldNames()
+        {   
+            List<XElement> AllFieldNames = new List<XElement>();
+
+            var fields = GetFieldDecs();
+
+            foreach (XElement field in fields)
+            {
+                var fieldname = field.Element(SRC.Declaration).Element(SRC.Name);                
+                if (fieldname != null)
+                    AllFieldNames.Add(fieldname);
+            }
+
+            return AllFieldNames.ToArray();
+        }
+
+        /// <summary>
+        /// Get all functions (including constructor and destructor) in the source file.
+        /// </summary>
+        /// <returns>An array (of XElement) that contains all the methods/constructors/destructors.</returns>
+        public XElement[] GetMethods()
+        {
+            List<XElement> Allmethods = new List<XElement>();
+
+            foreach (XElement file in srcmlFile.FileUnits)
+            {
+                var methods = from method in file.Descendants()
+                              where method.Name.Equals(SRC.Function)
+                             || method.Name.Equals(SRC.Constructor)
+                             || method.Name.Equals(SRC.Destructor)
+                             select method;
+
+                if (methods != null)
+                    Allmethods.AddRange(methods);
+            }
+
+            FieldDecs = Allmethods.ToArray();
+            return FieldDecs;
+        }
+
+        /// <summary>
+        /// Get names of all functions (including constructor and destructor) in the source file.
+        /// </summary>
+        /// <returns>An array (of "name" XElement) that contains names of all the functions.</returns>
+        public XElement[] GetMethodNames()
+        {
+            List<XElement> AllMethodNames = new List<XElement>();
+
+            var methods = GetMethods();
+
+            foreach (XElement method in methods)
+            {
+                var methodname = method.Element(SRC.Name);
+                if (methodname != null)
+                    AllMethodNames.Add(methodname);
+            }
+
+            return AllMethodNames.ToArray();
+        }
+
+        /// <summary>
+        /// Get all local variables declared in a given method XElement.
+        /// </summary>
+        /// <param name="srcPath">A given method in XElement format.</param>
+        public XElement[] GetAllLocalVarsinMethod(XElement method)
+        {
+            XElement methodbody = method.Element(SRC.Block);
+            var localdeclares =
+                methodbody.Elements(SRC.DeclarationStatement);
+               
+            List<XElement> localvars = new List<XElement>();
+            
+            foreach (var localdeclare in localdeclares)
+            {
+                XElement localvar = localdeclare.Element(SRC.Declaration).Element(SRC.Name);
+                localvars.Add(localvar);
+            }
+            
+            return localvars.ToArray();
+        }
+
+        /// <summary>
+        /// Get all parameters for a given method XElement.
+        /// </summary>
+        /// <param name="srcPath">A given method in XElement format.</param>
+        //public XElement[] GetAllParametersinMethod(XElement method)
+        //{
+
+        //}
+
+        //public XElement[] GetAllNonLocalVarsinMethod(XElement method)
+        //{
+        //    XElement methodbody = method.Element(SRC.Block);
+        //    var localdeclares =
+        //        methodbody.Elements(SRC.DeclarationStatement).Elements(SRC.Declaration);
+            
+        //    List<String> localvars = new List<String>();
+
+        //    foreach (var localdeclare in localdeclares)
+        //        localvars.Add(localdeclare.Element(SRC.Name).Value);
+
+        //    var allvars1 = from variable in methodbody.Descendants()
+        //                  where variable.Name.Equals(SRC.Name)
+        //                  && !variable.Ancestors(SRC.Type).Any()
+        //                  && !variable.Parent.Name.Equals(SRC.Call)                             
+        //                  select variable;
+
+        //    var allvars2 = from variable in methodbody.Descendants()
+        //                   where variable.Parent.Name.Equals(SRC.Call)
+        //                   where variable.Name.Equals(SRC.Name)
+        //                   select variable;
+
+        //    XElement allvars3;
+
+        //    foreach (var x in allvars2)
+        //    {
+        //        if (x.Elements(SRC.Name).Count() >= 2)
+        //            allvars3 = x.Element(SRC.Name);
+        //    }
+
+        //    List<XElement> nonlocalvars = new List<XElement>();
+        //    foreach (var variable in allvars) //allvars = allvars1 + allvars3
+        //    {
+        //        if (!(localvars.Contains(variable.Value)))
+        //            nonlocalvars.Add(variable);
+        //    }
+
+        //    return nonlocalvars.ToArray();
+
+
+        //}
+
+        /// <summary>
+        /// Determine if a field is used in a given method.
+        /// </summary>
+        /// <param name="srcPath">field and method</param>
+        /// <returns>true if it is used.</returns>
+        //public bool ifFieldUsedinMethod(XElement method, XElement field)
+        //{
+        //    var nonlocalvars = GetAllNonLocalVarsinMethod(method);
+        //    List<String> list_nonlocalvars = new List<string>();
+        //    foreach (var nonlocalvar in nonlocalvars)
+        //        list_nonlocalvars.Add(nonlocalvar.Value);
+
+        //    return (list_nonlocalvars.Contains(field.Value));
+        //}
+
+    }
+}
