ec410c79c5d45bcdf4ae3f2dd770583ffbf20ae5
xigeabb <devnull@localhost>
2013-06-10 15:08:37 -0400

    Add a thesaurus that is specific to the field of software engineering. Add a mechanism to find similar words , both semantically and structurally, of a word that does not exist in the local dictionary.

---
+++ Core/Core.UnitTests/Tools/DictionaryBasedQueryReformerTests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Sando.Core.Tools;
+
+namespace Sando.Core.UnitTests.Tools
+{
+    [TestFixture]
+    public class DictionaryBasedQueryReformerTests
+    {
+        private const String directory = @"TestFiles\";
+        private readonly DictionaryBasedSplitter dictionary;
+        private readonly DictionaryBasedQueryReformer reformer;
+
+        public DictionaryBasedQueryReformerTests()
+        {
+            this.dictionary = new DictionaryBasedSplitter();
+            this.reformer = new DictionaryBasedQueryReformer(dictionary);
+            dictionary.Initialize(directory);
+            reformer.Initialize();
+        }
+
+        private void AssertNotReformed(IEnumerable<string> words)
+        {
+            var newTermLists = reformer.ReformTermsSynchronously(words);
+            Assert.IsTrue(newTermLists.Count() == 1);
+            var changedTerms = newTermLists.First();
+            Assert.IsTrue(changedTerms.Count() == words.Count());
+            for (int i = 0; i < changedTerms.Count(); i++)
+            {
+                Assert.IsTrue(changedTerms.ElementAt(i).Equals(words.
+                    ElementAt(i)));
+            }
+        }
+
+        private void AssertReformed(String[] words, int[] changedIndexes)
+        {
+            var newTermLists = reformer.ReformTermsSynchronously(words).ToList();
+            Assert.IsTrue(newTermLists.Any());
+            foreach (List<string> newTermList in newTermLists)
+            {
+                var list = newTermList.ToList();
+                for (int i = 0; i < list.Count(); i++)
+                {
+                    if (changedIndexes.Contains(i))
+                    {
+                        Assert.IsTrue(!list.ElementAt(i).Equals(words.ElementAt(i)));
+                    }
+                    else
+                    {
+                        Assert.IsTrue(list.ElementAt(i).Equals(words.ElementAt(i)));
+                    }
+                }
+            }
+        }
+
+        [Test]
+        public void ReformWiredTerms()
+        {
+           reformer.ReformTermsSynchronously(new string[]{"", ""});
+        }
+
+        [Test]
+        public void ReformOneTermInDictionary()
+        {
+            AssertNotReformed(new string[] { "sando" });
+            AssertNotReformed(new string[] { "add" });
+            AssertNotReformed(new string[] { "adjust" });
+            AssertNotReformed(new string[] { "after" });
+            AssertNotReformed(new string[] { "alignment" });
+            AssertNotReformed(new string[] { "alignment","add","after"});
+        }
+
+        [Test]
+        public void ReformOneTermNotInDictionary()
+        {
+            AssertReformed(new string[] {"addi"}, new int[]{0});
+            AssertReformed(new string[] {"addi", "add"}, new int[]{0});
+            AssertReformed(new string[] {"add", "additional", "addi"}, new int[]{2});
+            AssertReformed(new string[] {"san"}, new int[]{0});
+            AssertReformed(new string[] {"adjusting"}, new int[]{0});
+            AssertReformed(new string[] {"aft"}, new int[]{0});
+            AssertReformed(new string[] {"alignme"}, new int[]{0});
+            AssertReformed(new string[] {"addi", "aft", "alignme"}, new int[]{0, 1, 2});
+        }       
+    }
+}
---
+++ Core/Core/Tools/DictionaryBasedQueryReformer.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Sando.Core.Tools
+{
+    /// <summary>
+    /// This is the listener when improved terms are ready.
+    /// </summary>
+    /// <param name="improvedTerms"></param>
+    public delegate void ImprovedTermsReady(IEnumerable<IEnumerable<String>> improvedTerms);
+
+    public class DictionaryBasedQueryReformer
+    {
+        const int SIMILAR_WORDS_MAX_COUNT = 2;
+        const int SYNONYMS_MAX_COUNT = 2;
+
+        private readonly DictionaryBasedSplitter dictionary;
+        private readonly IThesaurus seThesaurus;
+
+        public DictionaryBasedQueryReformer(DictionaryBasedSplitter dictionary)
+        {
+            this.dictionary = dictionary;
+            this.seThesaurus = SESpecificThesaurus.GetInstance();
+        }
+
+        public void Initialize()
+        {
+            ((SESpecificThesaurus)seThesaurus).Initialize();
+        }
+
+        public void ReformTermsAsynchronously(IEnumerable<String> terms, ImprovedTermsReady callback)
+        {
+            var worker = new BackgroundWorker { WorkerReportsProgress = false, 
+                WorkerSupportsCancellation = false };
+            worker.DoWork += (sender, args) => callback.Invoke(ReformTermsSynchronously(terms));
+            worker.RunWorkerAsync();
+        }
+
+        public IEnumerable<IEnumerable<string>> ReformTermsSynchronously(IEnumerable<string> terms)
+        {
+            var termList = terms.ToList();
+            if (termList.Any())
+            {
+                var results = new List<IEnumerable<String>>();
+                var betterFirstTerms = FindBetterTerms(termList.First()).ToList();
+                var restTermLists = ReformTermsSynchronously(termList.GetRange(1, termList.Count() 
+                    - 1)).ToList();           
+                foreach (String betterFirstTerm in betterFirstTerms)
+                {
+                    var list = new List<String> {betterFirstTerm};
+                    if (restTermLists.Any())
+                    {
+                        foreach (var restTermList in restTermLists)
+                        {
+                            var copyList = list.ToList();
+                            copyList.AddRange(restTermList);
+                            results.Add(copyList);
+                        }
+                    }
+                    else
+                    {
+                        results.Add(list);
+                    }
+                }
+                return results;
+            }
+            return Enumerable.Empty<IEnumerable<String>>();
+        }
+
+        private IEnumerable<String> FindBetterTerms(String word)
+        {
+            if (!dictionary.DoesWordExist(word))
+            {
+                var list = new List<String>();
+                list.AddRange(FindShapeSimilarWordsInLocalDictionary(word));
+                list.AddRange(FindSynonymsInLocalDictionary(word));
+                return list;
+            }
+            return new []{word};
+        }
+
+        private IEnumerable<String> FindShapeSimilarWordsInLocalDictionary(String word)
+        {
+            var list = dictionary.FindSimilarWords(word).ToList();
+            if (list.Count >= SIMILAR_WORDS_MAX_COUNT)
+                list = list.GetRange(0, SIMILAR_WORDS_MAX_COUNT);
+            return list;
+        }
+
+        private IEnumerable<String> FindSynonymsInLocalDictionary(String word)
+        {
+            var synonyms = seThesaurus.GetSynonyms(word);
+            var list = synonyms.Where(w => dictionary.DoesWordExist(w)).ToList();
+            if (list.Count() >= SYNONYMS_MAX_COUNT)
+                list = list.GetRange(0, SYNONYMS_MAX_COUNT);
+            return list;
+        }
+    }
+}
