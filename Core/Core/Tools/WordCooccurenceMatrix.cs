6db64ce1d612eda6aaa250ca57c1c8e1d21cda34
xigeabb <devnull@localhost>
2013-06-11 14:52:42 -0400

    Add coocurrence table file, not implemented yet.

---
+++ Core/Core/Tools/WordCooccurenceMatrix.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sando.Core.Tools
+{
+    public class WordCooccurenceMatrix
+    { 
+
+        public WordCooccurenceMatrix()
+        {
+        }
+    }
+}
