1a483312791a3a57eb36a8c9207d667c81c6c459
lordlothar <devnull@localhost>
2013-06-17 23:28:24 +0200

    Adding SandoQueryParser

---
+++ Core/Core.UnitTests/Tools/QueryParserTests.cs
@@ -0,0 +1,112 @@
+﻿using NUnit.Framework;
+using Sando.Core.Tools;
+
+namespace Sando.Core.UnitTests.Tools
+{
+    [TestFixture]
+    public class QueryParserTests
+    {
+        [Test]
+        [TestCaseSource("InvalidQueryTestCases")]
+        public void GIVEN_QueryParser_WHEN_ParseIsCalled_AND_QueryIsNullOrEmptyStringOrContainsWhiteSpaceOnly_THAN_ValidQueryDescriptionIsReturned(string query)
+        {
+            var sandoQueryParser = new SandoQueryParser();
+            var sandoQueryDescription = sandoQueryParser.Parse(null);
+
+            Assert.IsFalse(sandoQueryDescription.IsValid);
+        }
+
+        [Test]
+        [TestCaseSource("ValidLiteralQueryTestCases")]
+        public void GIVEN_QueryParser_WHEN_ParseIsCalled_AND_QueryIsLiteralString_THAN_ValidQueryDescriptionIsReturned(string query, string expectedQueryDescription)
+        {
+            var sandoQueryParser = new SandoQueryParser();
+            var sandoQueryDescription = sandoQueryParser.Parse(query);
+
+            Assert.IsTrue(sandoQueryDescription.IsValid);
+            Assert.AreEqual(expectedQueryDescription, sandoQueryDescription.ToString());
+        }
+
+        [Test]
+        [TestCaseSource("ValidFileExtensionFiltersTestCases")]
+        public void GIVEN_QueryParser_WHEN_ParseIsCalled_AND_QueryIsFileExtensionString_THAN_ValidQueryDescriptionIsReturned(string query, string expectedQueryDescription)
+        {
+            var sandoQueryParser = new SandoQueryParser();
+            var sandoQueryDescription = sandoQueryParser.Parse(query);
+
+            Assert.IsTrue(sandoQueryDescription.IsValid);
+            Assert.AreEqual(expectedQueryDescription, sandoQueryDescription.ToString());
+        }
+
+        [Test]
+        [TestCaseSource("ValidProgramElementTypeFiltersTestCases")]
+        public void GIVEN_QueryParser_WHEN_ParseIsCalled_AND_QueryIsProgramElementTypeString_THAN_ValidQueryDescriptionIsReturned(string query, string expectedQueryDescription)
+        {
+            var sandoQueryParser = new SandoQueryParser();
+            var sandoQueryDescription = sandoQueryParser.Parse(query);
+
+            Assert.IsTrue(sandoQueryDescription.IsValid);
+            Assert.AreEqual(expectedQueryDescription, sandoQueryDescription.ToString());
+        }
+
+        [Test]
+        [TestCaseSource("ValidAccessLevelFiltersTestCases")]
+        public void GIVEN_QueryParser_WHEN_ParseIsCalled_AND_QueryIsAccessLevelString_THAN_ValidQueryDescriptionIsReturned(string query, string expectedQueryDescription)
+        {
+            var sandoQueryParser = new SandoQueryParser();
+            var sandoQueryDescription = sandoQueryParser.Parse(query);
+
+            Assert.IsTrue(sandoQueryDescription.IsValid);
+            Assert.AreEqual(expectedQueryDescription, sandoQueryDescription.ToString());
+        }
+
+
+        private static readonly object[] InvalidQueryTestCases =
+            {
+                "",
+                " ",
+                "\t",
+                "\n",
+                "\t  \n",
+                "\"  \""
+            };
+
+        private static readonly object[] ValidLiteralQueryTestCases =
+            {
+                new object[]{"\"asd^&*#@$%!()_+-=:';[]{}?/><,.żźćąśęńłó\"",             "Literal search terms:[\"asd^&*#@$%!()_+-=:';[]{}?/><,.żźćąśęńłó\"]"},
+                new object[]{"\"sample literal search\"",                               "Literal search terms:[\"sample literal search\"]"},
+                new object[]{"\"sample literal search with escaped quote \\\"\"",       "Literal search terms:[\"sample literal search with escaped quote \\\"\"]"},
+                new object[]{"\"\\\"\"",                                                "Literal search terms:[\"\\\"\"]"},
+                new object[]{"\"var sandoQueryParser = new SandoQueryParser();\"",      "Literal search terms:[\"var sandoQueryParser = new SandoQueryParser();\"]"},
+                new object[]{"\"var sandoQueryParser\" \"new SandoQueryParser();\"",    "Literal search terms:[\"var sandoQueryParser\",\"new SandoQueryParser();\"]"},
+                new object[]{"\"name \\\"\" \" sandoQueryParser\" \"new Sando\"",       "Literal search terms:[\"name \\\"\",\" sandoQueryParser\",\"new Sando\"]"},
+                new object[]{"-\"name\" \" \t\n \"",                                    "Literal search terms:[-\"name\"]"},
+                new object[]{"-\"name \\\"\" \" -sandoQueryParser\" -\"new Sando\"",    "Literal search terms:[-\"name \\\"\",\" -sandoQueryParser\",-\"new Sando\"]"},
+                new object[]{"\"open\" file\"",                                         "Literal search terms:[\"open\"]"}
+            };
+
+        private static readonly object[] ValidFileExtensionFiltersTestCases =
+            {
+                new object[]{"file:cs",                 "File extensions:[cs]"},
+                new object[]{"file:long_entension",     "File extensions:[long_entension]"},
+                new object[]{"-file:mp3",               "File extensions:[-mp3]"},
+                new object[]{"file:txt -file:dat",      "File extensions:[txt,-dat]"}
+            };
+
+        private static readonly object[] ValidProgramElementTypeFiltersTestCases =
+            {
+                new object[]{"type:class",                  "Program element types:[class]"},
+                new object[]{"type:method",                 "Program element types:[method]"},
+                new object[]{"-type:enum type:field",       "Program element types:[-enum,field]"},
+                new object[]{"type:property -type:struct",  "Program element types:[property,-struct]"}
+            };
+
+        private static readonly object[] ValidAccessLevelFiltersTestCases =
+            {
+                new object[]{"access:public",                       "Access levels:[public]"},
+                new object[]{"access:private",                      "Access levels:[private]"},
+                new object[]{"-access:protected access:internal",   "Access levels:[-protected,internal]"},
+                new object[]{"access:protected -access:public",     "Access levels:[protected,-public]"}
+            };
+    }
+}
---
+++ Core/Core/Tools/SandoQueryParser.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Sando.Core.Tools
+{
+    public class SandoQueryParser
+    {
+        private const string LiteralSearchRegex = @"(?<literal>\-?""(?<literalcontent>[^""\\]*(?:\\.[^""\\]*)*)"")";
+        private const string FileExtensionFilterRegex = @"(?<filter>\-?file:(?<fileext>\w+))";
+        private const string ProgramElementTypeFilterRegex = @"(?<filter>\-?type:(?<type>field|method|property|enum|struct|class))";
+        private const string AccessLevelFilterRegex = @"(?<filter>\-?access:(?<access>public|private|protected|internal))";
+
+        public SandoQueryDescription Parse(string query)
+        {
+            var sandoQueryDescription = new SandoQueryDescription();
+            var parseFunctions = GetQueryParseFunctions();
+            foreach (var function in parseFunctions)
+            {
+                if (String.IsNullOrWhiteSpace(query))
+                    return sandoQueryDescription;
+                query = function(query, sandoQueryDescription);
+            }
+            return sandoQueryDescription;
+        }
+
+        private static IEnumerable<Func<string, SandoQueryDescription, string>> GetQueryParseFunctions()
+        {
+            return new List<Func<string, SandoQueryDescription, string>>
+                {
+                    ParseLiteral,
+                    ParseFileExtensionFilters,
+                    ParseProgramElementTypeFilters,
+                    ParseAccessLevelFilters
+                };
+        }
+
+        private static string ParseLiteral(string query, SandoQueryDescription sandoQueryDescription)
+        {
+            var matches = Regex.Matches(query, LiteralSearchRegex);
+            foreach (Match match in matches)
+            {
+                var matchedLiteral = match.Groups["literal"].Value;
+                var matchedLiteralContent = match.Groups["literalcontent"].Value;
+                if (!String.IsNullOrWhiteSpace(matchedLiteralContent))
+                {
+                    sandoQueryDescription.LiteralSearchTerms.Add(matchedLiteral);
+                }
+                query = query.Replace(matchedLiteral, String.Empty);
+            }
+            query = query.Replace("\"", String.Empty);
+            return query;
+        }
+
+        private static string ParseFileExtensionFilters(string query, SandoQueryDescription sandoQueryDescription)
+        {
+            var matches = Regex.Matches(query, FileExtensionFilterRegex);
+            foreach (Match match in matches)
+            {
+                var matchedFilter = match.Groups["filter"].Value;
+                var matchedFileExtension = match.Groups["fileext"].Value;
+                if (matchedFilter.StartsWith("-"))
+                    sandoQueryDescription.FileExtensions.Add("-" + matchedFileExtension);
+                else
+                    sandoQueryDescription.FileExtensions.Add(matchedFileExtension);
+                query = query.Replace(matchedFilter, String.Empty);
+            }
+            query = query.Replace("\"", String.Empty);
+            return query;
+        }
+
+        private static string ParseProgramElementTypeFilters(string query, SandoQueryDescription sandoQueryDescription)
+        {
+            var matches = Regex.Matches(query, ProgramElementTypeFilterRegex);
+            foreach (Match match in matches)
+            {
+                var matchedFilter = match.Groups["filter"].Value;
+                var matchedProgramElementType = match.Groups["type"].Value;
+                if (matchedFilter.StartsWith("-"))
+                    sandoQueryDescription.ProgramElementTypes.Add("-" + matchedProgramElementType);
+                else
+                    sandoQueryDescription.ProgramElementTypes.Add(matchedProgramElementType);
+                query = query.Replace(matchedFilter, String.Empty);
+            }
+            query = query.Replace("\"", String.Empty);
+            return query;
+        }
+
+        private static string ParseAccessLevelFilters(string query, SandoQueryDescription sandoQueryDescription)
+        {
+            var matches = Regex.Matches(query, AccessLevelFilterRegex);
+            foreach (Match match in matches)
+            {
+                var matchedFilter = match.Groups["filter"].Value;
+                var matchedAccessLevel = match.Groups["access"].Value;
+                if (matchedFilter.StartsWith("-"))
+                    sandoQueryDescription.AccessLevels.Add("-" + matchedAccessLevel);
+                else
+                    sandoQueryDescription.AccessLevels.Add(matchedAccessLevel);
+                query = query.Replace(matchedFilter, String.Empty);
+            }
+            query = query.Replace("\"", String.Empty);
+            return query;
+        }
+    }
+
+    public class SandoQueryDescription
+    {
+        public SandoQueryDescription()
+        {
+            SearchTerms = new List<string>();
+            LiteralSearchTerms = new List<string>();
+            FileExtensions = new List<string>();
+            ProgramElementTypes = new List<string>();
+            WildcardSearchTerms = new List<string>();
+            Locations = new List<string>();
+            AccessLevels = new List<string>();
+        }
+
+        public List<string> SearchTerms { get; set; }
+        public List<string> LiteralSearchTerms { get; set; }
+        public List<string> FileExtensions { get; set; }
+        public List<string> ProgramElementTypes { get; set; }
+        public List<string> WildcardSearchTerms { get; set; }
+        public List<string> Locations { get; set; }
+        public List<string> AccessLevels { get; set; }
+
+        public bool IsValid
+        {
+            get
+            {
+                return SearchTerms.Any() ||
+                       LiteralSearchTerms.Any() ||
+                       FileExtensions.Any() ||
+                       ProgramElementTypes.Any() ||
+                       WildcardSearchTerms.Any() ||
+                       Locations.Any() ||
+                       AccessLevels.Any();
+            }
+        }
+
+        public override string ToString()
+        {
+            return String.Join(", ", GetDescriptionForCollections().Where(d => d != null));
+        }
+
+        private IEnumerable<string> GetDescriptionForCollections()
+        {
+            return new List<string>
+                {
+                    GetDescriptionForCollection("Search terms", SearchTerms),
+                    GetDescriptionForCollection("Literal search terms", LiteralSearchTerms),
+                    GetDescriptionForCollection("File extensions", FileExtensions),
+                    GetDescriptionForCollection("Program element types", ProgramElementTypes),
+                    GetDescriptionForCollection("Wildcard search terms", WildcardSearchTerms),
+                    GetDescriptionForCollection("Locations", Locations),
+                    GetDescriptionForCollection("Access levels", AccessLevels)
+                };
+        }
+
+        private static string GetDescriptionForCollection(string collectionName, List<string> collection)
+        {
+            return collection.Any() ? String.Format("{0}:[{1}]", collectionName,  String.Join(",", collection)) : null;
+        }
+    }
+}
