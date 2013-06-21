e9ccdf13920291e1f70b3dbb1fb5c9801aa76321
xigeabb <devnull@localhost>
2013-06-21 14:30:17 -0400

    Move recommended query UI to a new file.

--- UI/UI/View/SearchViewControl.xaml.cs
+++ UI/UI/View/SearchViewControl.xaml.cs
@@ -371,63 +371,7 @@ namespace Sando.UI.View
             }
         }
 
-        public void UpdateRecommendedQueries(IQueryable<String> queries)
-        {
-            queries = ControlRecommendedQueriesCount(queries);
-            if (Thread.CurrentThread == Dispatcher.Thread)
-            {
-                InternalUpdateRecommendedQueries(queries);
-            }
-            else
-            {
-                Dispatcher.Invoke((Action)(() => 
-                    InternalUpdateRecommendedQueries(queries)));
-            }
-        }
-
-        private static IQueryable<string> ControlRecommendedQueriesCount(IQueryable<string> queries)
-        {
-            if (queries.Count() > QuerySuggestionConfigurations.
-                MAXIMUM_RECOMMENDED_QUERIES_IN_USER_INTERFACE)
-            {
-                queries = queries.ToList().GetRange(0, QuerySuggestionConfigurations
-                    .MAXIMUM_RECOMMENDED_QUERIES_IN_USER_INTERFACE).AsQueryable();
-            }
-            return queries;
-        }
-
-        private void InternalUpdateRecommendedQueries(IEnumerable<string> quries)
-        {
-            RecommendedQueryTextBlock.Inlines.Clear();
-            foreach (string qury in quries)
-            {
-                var hyperlink = new SandoQueryHyperLink(new Run(qury), qury);
-                hyperlink.Click += HyperlinkOnClick;
-                RecommendedQueryTextBlock.Inlines.Add(hyperlink);
-                RecommendedQueryTextBlock.Inlines.Add("  ");
-            }
-        }
-
-        private class SandoQueryHyperLink : Hyperlink
-        {
-            public String Query { private set; get; }
-
-            internal SandoQueryHyperLink(Run run, String query) : base(run)
-            {
-                this.Query = query;
-            }
-        }
-
-        private void HyperlinkOnClick(object sender, RoutedEventArgs routedEventArgs)
-        {
-            if (sender as SandoQueryHyperLink != null)
-            {
-                var reformedQuery = (sender as SandoQueryHyperLink).Query;
-                searchBox.Text = reformedQuery;
-                BeginSearch(reformedQuery);
-            }
-        }
-
+     
 
         private void UpdateExpansionState(ListView view)
         {
