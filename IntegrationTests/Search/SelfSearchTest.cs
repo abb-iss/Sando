d0f7c5e8df721c879f8b4cc6b094cbe43625fbbf
davidcshepherd <devnull@localhost>
2013-07-03 14:07:59 -0400

    Updating test...

--- IntegrationTests/Search/SelfSearchTest.cs
+++ IntegrationTests/Search/SelfSearchTest.cs
@@ -28,7 +28,7 @@ namespace Sando.IntegrationTests.Search
         public void FindBodyText()
         {
             string keywords = "document add";
-            var expectedLowestRank = 6;
+            var expectedLowestRank = 7;
             Predicate<CodeSearchResult> predicate = el => el.ProgramElement.ProgramElementType == ProgramElementType.Method && (el.ProgramElement.Name == "GetLuceneDocument");
             EnsureRankingPrettyGood(keywords, predicate, expectedLowestRank);
         }
