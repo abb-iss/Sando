7d38c3466806e65fa9d9c0e3099ab2f7e44980f6
xigeabb <devnull@localhost>
2013-07-10 16:54:51 -0400

    Add stop words list.

---
+++ Core/Core/Tools/SpecialWords.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sando.Core.Tools
+{
+    public class SpecialWords
+    {
+        public static String[] NonInformativeWords()
+        {
+            return new string[]{};
+        }
+    }
+}
