5d3985919b2928eff429ce597230d5a6fa886298
xigeabb <devnull@localhost>
2013-07-11 16:09:43 -0400

    Add several words.

--- Core/Core/Tools/SpecialWords.cs
+++ Core/Core/Tools/SpecialWords.cs
@@ -24,21 +24,21 @@ namespace Sando.Core.Tools
                 as	explicit	null	switch
                 base	extern	object	this
                 bool	false	operator	throw
-                break	finally	out	true
+                break	finally	out	true system
                 byte	fixed	override	try
                 case	float	params	typeof
-                catch   for	private	uint
+                catch   for	private	uint main
                 char    foreach	protected	ulong
                 checked	goto	public	unchecked
                 class	if	readonly	unsafe
                 const	implicit	ref	ushort
-                continue	in	return	using
-                decimal	int	sbyte	virtual
+                continue	in	return	using error
+                decimal	int	sbyte	virtual warning
                 default	interface	sealed	volatile
                 delegate	internal	short	void
-                do	is	sizeof	while
-                double	lock	stackalloc	 
-                else    long	static	 
+                do	is	sizeof	while debug info
+                double	lock	stackalloc logger
+                else    long	static exception
                 enum namespace string var".Split();
         }
 
