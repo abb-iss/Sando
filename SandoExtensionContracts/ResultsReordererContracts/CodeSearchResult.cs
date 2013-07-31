b069aa956de6977d7605fcc87f83291f3ecdb055
xigeabb <devnull@localhost>
2013-07-31 17:19:44 -0400

    Add line number to the code snippet pop up menu.

--- SandoExtensionContracts/ResultsReordererContracts/CodeSearchResult.cs
+++ SandoExtensionContracts/ResultsReordererContracts/CodeSearchResult.cs
@@ -8,6 +8,13 @@ using System.Text;
 
 namespace Sando.ExtensionContracts.ResultsReordererContracts
 {
+    public interface IHighlightRawInfo
+    {
+        string HighlightRaw { get; }
+        int StartLineNumber { get; }
+    }
+
+
     /// <summary>
     /// Class defined to create return result from Lucene indexer
     /// </summary>
@@ -104,14 +111,37 @@ namespace Sando.ExtensionContracts.ResultsReordererContracts
         }
         //Zhao Li, return all the seach results
         private string highlight;
-        public string Highlight {
+        public String Highlight
+        {
             get {
-                return this.highlight;
+                return highlight;
             }
             set {
                 this.highlight = value;
             }
         }
+
+
+        public IHighlightRawInfo HighlightInfo
+        {
+            get
+            {
+                return new InternalHighlightRawInfo(highlight, ProgramElement.DefinitionLineNumber);
+            }
+        }
+
+        private class InternalHighlightRawInfo : IHighlightRawInfo
+        {
+            public string HighlightRaw { get; private set; }
+            public int StartLineNumber { get; private set; }
+          
+            internal InternalHighlightRawInfo(String HighlightRaw, int StartLineNumber)
+            {
+                this.HighlightRaw = HighlightRaw;
+                this.StartLineNumber = StartLineNumber;
+            }
+        }
+
 
         private static int TAB = 4;
         private static int MAX_SNIPPET_LENGTH = 85;        
@@ -126,6 +156,7 @@ namespace Sando.ExtensionContracts.ResultsReordererContracts
             var lines = GetLines(source);
             int leadingSpaces = 0;
             var shortenedLineList = ShortenSnippet(numLines, lines);
+
             
             if (!IsXml())
             {
@@ -170,14 +201,39 @@ namespace Sando.ExtensionContracts.ResultsReordererContracts
             return snippet;
         }
 
+        private int[] CalculateStartAndEndLine()
+        {
+            var resultLines = highlightRaw.Split('\n').Where(n => !string.IsNullOrWhiteSpace(n)).ToList();
+            var allLines = File.ReadAllLines(ProgramElement.FullFilePath).ToList();
+            var lineCount = resultLines.Count();
+            int start;
+            for (start = 0; !AreAllLinesSame(resultLines, allLines.GetRange(start, lineCount)); start ++);
+            return new []{start, start + lineCount - 1};
+        }
+
+        private bool AreAllLinesSame(IEnumerable<string> lines1, IEnumerable<string> lines2)
+        {
+            lines1 = lines1.ToArray();
+            lines2 = lines2.ToArray();
+            if (lines1.Count() == lines2.Count())
+            {
+                for (int i = 0; i < lines1.Count(); i++)
+                {
+                    if (!lines1.ElementAt(i).Trim().Equals(lines2.ElementAt(i).Trim()))
+                        return false;
+                }
+                return true;
+            }
+            return false;
+        }
+
         private static List<string> StandardizeLeadingWhitespace(List<string> lines, int numLines)
         {
             var newLines = new List<string>();
             int count = 0;
-            var line = "";
             foreach (var aLine in lines)
             {
-                line = aLine;
+                var line = aLine;
                 if (!line.Trim().Equals(""))
                 {
                     while (line.StartsWith(" ") || line.StartsWith("\t"))
--- UI/UI/View/Search/Converters/HighlightSearchKey.cs
+++ UI/UI/View/Search/Converters/HighlightSearchKey.cs
@@ -11,80 +11,103 @@ using System.Windows.Markup;
 using System.Windows;
 using System.Windows.Documents;
 using System.Text.RegularExpressions;
+using Sando.ExtensionContracts.ResultsReordererContracts;
 
 namespace Sando.UI.View.Search.Converters {
-     [ValueConversion(typeof(string), typeof(object))]
+     [ValueConversion(typeof(IHighlightRawInfo), typeof(object))]
     class HighlightSearchKey : IValueConverter {
-        public Object Convert(Object value, Type targetType,
-                      object parameter, CultureInfo culture) {
-                          var span = new Span();
-                          try
-                          {
-                              // return null;
-                              if (value == null)
-                              {
-                                  return null;
-                              }
 
-                              string input = value as string;
-                              string[] lines = input.Split('\n');
+         private String GetHighlightRaw(object inforvalue)
+         {
+             if (inforvalue as IHighlightRawInfo == null)
+                 return (String) inforvalue;
+             return (inforvalue as IHighlightRawInfo).HighlightRaw;
+         }
+
+         private RunsLine[] AddLineNumber(object inforvalue, RunsLine[] lines)
+         {
+             if (inforvalue as IHighlightRawInfo != null)
+             {
+                 var startNum = (inforvalue as IHighlightRawInfo).StartLineNumber;
+                 foreach (var line in lines)
+                 {
+                     line.AddRunFromBeginning(new Run(startNum.ToString() + ":\t"));
+                     startNum++;
+                 }
+             }
+             return lines;
+         }
+
+        public Object Convert(Object inforValue, Type targetType, object parameter, CultureInfo culture)
+        {
+            var value = GetHighlightRaw(inforValue);
+            var span = new Span();
+            try
+            {
+                // return null;
+                if (value == null)
+                {
+                    return null;
+                }
+
+                string input = value as string;
+                string[] lines = input.Split('\n');
                               
-                              List<string> key_temp = new List<string>();
+                List<string> key_temp = new List<string>();
 
-                              foreach (string line in lines)
-                              {
+                foreach (string line in lines)
+                {
 
-                                  if (line.Contains("|~S~|"))
-                                  {
+                    if (line.Contains("|~S~|"))
+                    {
 
-                                      string findKey = string.Copy(line);
+                        string findKey = string.Copy(line);
 
-                                      while (findKey.IndexOf("|~S~|") >= 0)
-                                      {
-                                          int first = findKey.IndexOf("|~S~|") + "|~S~|".Length;
-                                          int last = findKey.IndexOf("|~E~|");
+                        while (findKey.IndexOf("|~S~|") >= 0)
+                        {
+                            int first = findKey.IndexOf("|~S~|") + "|~S~|".Length;
+                            int last = findKey.IndexOf("|~E~|");
 
-                                          string key_candidate = findKey.Substring(first, last - first);
+                            string key_candidate = findKey.Substring(first, last - first);
 
-                                          bool removed = false;
-                                          if (key_candidate.StartsWith("|~S~|"))
-                                          {
-                                              removed = true;
-                                              key_candidate = key_candidate.Remove("|~S~|".Length);
-                                          }
+                            bool removed = false;
+                            if (key_candidate.StartsWith("|~S~|"))
+                            {
+                                removed = true;
+                                key_candidate = key_candidate.Remove("|~S~|".Length);
+                            }
 
 
-                                          if (!key_temp.Contains(key_candidate))
-                                              key_temp.Add(key_candidate);
+                            if (!key_temp.Contains(key_candidate))
+                                key_temp.Add(key_candidate);
 
-                                          //Remove the searched string
-                                          int lengthRemove = last - first + 2 * "|~S~|".Length;
-                                          findKey = findKey.Remove(first - "|~S~|".Length, lengthRemove);
-                                          if (removed)
-                                              findKey = findKey.Insert(first - "|~S~|".Length, "|~S~|");
-                                      }
+                            //Remove the searched string
+                            int lengthRemove = last - first + 2 * "|~S~|".Length;
+                            findKey = findKey.Remove(first - "|~S~|".Length, lengthRemove);
+                            if (removed)
+                                findKey = findKey.Insert(first - "|~S~|".Length, "|~S~|");
+                        }
 
-                                      string[] key = key_temp.ToArray();
-                                      string[] temp = line.Split(new[] { "|~S~|", "|~E~|" }, StringSplitOptions.RemoveEmptyEntries);
+                        string[] key = key_temp.ToArray();
+                        string[] temp = line.Split(new[] { "|~S~|", "|~E~|" }, StringSplitOptions.RemoveEmptyEntries);
 
-                                      foreach (string item in temp)
-                                      {
-                                          span.Inlines.Add(IsSearchKey(item, key)
-                                            ? new Run(item) {FontWeight = FontWeights.Bold}
-                                                : new Run(item));
-                                      }
-                                  }
-                                  else
-                                      span.Inlines.Add(new Run(line));
-                                  span.Inlines.Add(new Run("\n"));
-                              }
-                              return ClearSpan(span);
-                          }
-                          catch (Exception e)
-                          {
-                              return ClearSpan(span);
-                          }
-
+                        foreach (string item in temp)
+                        {
+                            span.Inlines.Add(IsSearchKey(item, key)
+                            ? new Run(item) {FontWeight = FontWeights.Bold}
+                                : new Run(item));
+                        }
+                    }
+                    else
+                        span.Inlines.Add(new Run(line));
+                    span.Inlines.Add(new Run("\n"));
+                }
+                return ClearSpan(inforValue, span);
+            }
+            catch (Exception e)
+            {
+                return ClearSpan(inforValue, span);
+            }
         }
 
         private bool IsSearchKey(string input, string[] keyset) {
@@ -95,32 +118,45 @@ namespace Sando.UI.View.Search.Converters {
             return false;
         }
 
-         private Span ClearSpan(Span span)
+         private Span ClearSpan(object inforValue, Span span)
          {
-             var runs = RemoveEmptyLines(span.Inlines.Cast<Run>()).ToArray();
+             var runs = RemoveEmptyLines(inforValue, 
+                 span.Inlines.Cast<Run>()).ToArray();
              span.Inlines.Clear();
              span.Inlines.AddRange(runs);
              return span;
          }
 
 
-        private IEnumerable<Run> RemoveEmptyLines(IEnumerable<Run> terms)
+         private IEnumerable<Run> RemoveEmptyLines(object inforValue, IEnumerable<Run> terms)
         {
             var runs = new List<Run>();
-            var lines = BreakToRunLines(terms).Select(l => l.RemoveEmptyRun()).
-                Where(l => !l.IsEmpty()).ToArray();
-            lines = AlignIndention(lines);
-
-
+            var lines = BreakToRunLines(terms).Select(l => l.RemoveEmptyRun()).ToArray();
+            lines = AddLineNumber(inforValue, RemoveHeadingAndTrailingEmptyLines(AlignIndention(lines)));
             foreach (var line in lines)
             {
                 runs.AddRange(line.GetRuns());
                 runs.Add(new Run(Environment.NewLine));
             }
             return runs.Any() && runs.Last().Text.Equals(Environment.NewLine)
-                    ? runs.GetRange(0, runs.Count() - 1)
-                       : runs;
+                ? runs.GetRange(0, runs.Count() - 1)
+                    : runs;
         }
+
+         private RunsLine[] RemoveHeadingAndTrailingEmptyLines(IEnumerable<RunsLine> lines)
+         {
+             var list = lines.ToList();
+             while (list.Any() && String.IsNullOrWhiteSpace(list.First().GetLine()))
+             {
+                 list.RemoveAt(0);
+             }
+             while (list.Any() && String.IsNullOrWhiteSpace(list.Last().GetLine()))
+             {
+                 list.RemoveAt(list.Count - 1);
+             }
+             return list.ToArray();
+         }
+
 
          private RunsLine[] AlignIndention(RunsLine[] lines)
          {
