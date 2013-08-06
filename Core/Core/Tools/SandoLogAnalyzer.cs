982279e3bb7fb64d2ffa3fb16392ccc75062af55
xigeabb <devnull@localhost>
2013-08-05 22:44:41 -0400

    Add log file analyzers.

--- Core/Core/Tools/SandoLogAnalyzer.cs
+++ Core/Core/Tools/SandoLogAnalyzer.cs
@@ -22,7 +22,6 @@ namespace Sando.Core.Tools
     {
         private readonly SandoLogAnalyzer analyzer;
 
-
         public SandoAnalysisManager(string directory)
         {
             this.analyzer = new SandoLogAnalyzer(directory);
@@ -31,6 +30,7 @@ namespace Sando.Core.Tools
         public void Analyze()
         {
             this.analyzer.AddAnalyzer(new NoSearchResultsAnalyzer());
+            this.analyzer.AddAnalyzer(new NumberOfUsersAnalyzer());
             this.analyzer.StartAnalysis();
         }
         
@@ -55,6 +55,30 @@ namespace Sando.Core.Tools
                 string result = sb.ToString();
             }
         }
+
+        private class NumberOfUsersAnalyzer : ILogFileAnalyzer
+        {
+            private readonly Dictionary<string, int> IDs = new Dictionary<string, int>();  
+
+            public void StartAnalyze(ILogFile file)
+            {
+                var id = file.Name.Split('_')[2];
+                if (IDs.ContainsKey(id))
+                {
+                    IDs[id]++;
+                }
+                else
+                {
+                    IDs.Add(id, 1);
+                }
+            }
+
+            public void FinishAnalysis()
+            {
+                int count = IDs.Keys.Count;
+            }
+        }
+
 
 
         public class SandoLogAnalyzer
@@ -102,7 +126,14 @@ namespace Sando.Core.Tools
 
             private bool IsFileNameGood(String fileName)
             {
-                return true;
+                var shepherd = "2021486822";
+                var xi = "1914121570";
+                var busy = "1602809067";
+                var pat = "174697094";
+                var vinay = "222472157";
+
+                var list = new List<String> {shepherd, xi, busy, pat, vinay};
+                return list.All(l => !fileName.Contains(l));
             }
 
 
