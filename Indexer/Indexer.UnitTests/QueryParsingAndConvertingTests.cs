a593c180c1927234dfe4d5bfe355f61802e9435c
David C. Shepherd <david.shepherd@us.abb.com>
2013-08-14 10:05:33 -0400

    Adding test case to cover the bug a user reported today regarding a search query for "g_u16ActiveFault"

---
+++ Indexer/Indexer.UnitTests/QueryParsingAndConvertingTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Sando.Core.Tools;
+using Sando.Indexer.Searching.Criteria;
+
+namespace Sando.Indexer.UnitTests
+{
+    [TestFixture]
+    public class QueryParsingAndConvertingTests
+    {
+
+        [Test]
+        public void TestIfQueryParsesToEmptySearchTerm()
+        {
+            var description = new SandoQueryParser().Parse("g_u16ActiveFault");
+            var builder = CriteriaBuilder.GetBuilder().AddFromDescription(description);
+            var simple = builder.GetCriteria() as SimpleSearchCriteria;
+            Assert.IsFalse(simple.SearchTerms.Where(x => String.IsNullOrWhiteSpace(x)).ToList().Count >= 1);
+        }
+    }
+}
